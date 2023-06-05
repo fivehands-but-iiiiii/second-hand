9eac98a3bca69831d54f42d2b6efcf57656eedf0
SONG <geun2121@gmail.com>
2023-06-05 17:43:28 +0900

    [#5] FEAT: 각 탭에 대한 ViewController 파일 생성
    - 각 탭에 할당할 NavigationController

---
+++ iOS/second-hand/second-hand/ChattingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChattingViewController.swift
+//  second-hand
+//
+//  Created by SONG on 2023/06/05.
+//
+
+import UIKit
+
+class ChattingViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
