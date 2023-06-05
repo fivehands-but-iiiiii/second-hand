91092182f07ed84b347fbe3a6cf637c7306e0af2
SONG <geun2121@gmail.com>
2023-06-05 17:44:58 +0900

    [#5] FEAT: TabBarItem 세팅
    - 각 탭에 대한 item 생성 
    - 탭에 NaviController 할당

--- iOS/second-hand/second-hand/SecondHandTabBarController.swift
+++ iOS/second-hand/second-hand/SecondHandTabBarController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class SecondHandTabBarController: UITabBarController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        addViewController()
     }
     
-    
+    private func addViewController() {
+        let list : [UIViewController] = [HomeViewController(),SaleLogViewController(),InterestingListViewController(),ChattingViewController(),MyAccountViewController()]
+        for index in 0...4 {
+            list[index].tabBarItem = TabBarItems.setItems()[index]
+        }
+        self.setViewControllers(list, animated: true)
+    }
 }
 
 
