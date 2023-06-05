4645cbac10c0bed4bb023eec4ef3fdc3df1cb12c
SONG <geun2121@gmail.com>
2023-06-05 15:54:15 +0900

    [#5] FEAT: 새로운 RootViewController 지정
    - TabBarController를 rootViewController로 설정

---
+++ iOS/second-hand/second-hand/SecondHandTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondHandTabBarController.swift
+//  second-hand
+//
+//  Created by SONG on 2023/06/05.
+//
+
+import UIKit
+
+class SecondHandTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
+
