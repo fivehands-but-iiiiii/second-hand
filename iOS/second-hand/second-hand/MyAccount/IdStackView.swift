2c25375b638bff27d13cec0196a7d0587025feae
Haena <ghkwlsspdl@naver.com>
2023-06-07 16:21:08 +0900

    [#9] FEAT: idTextField 세팅
    - idTextField 세팅
    - idTextField 레이아웃

--- iOS/second-hand/second-hand/MyAccount/IdStackView.swift
+++ iOS/second-hand/second-hand/MyAccount/IdStackView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class IdStackView: UIStackView {
     let idLabel = UILabel()
+    let idTextField = UITextField()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,17 +23,23 @@ class IdStackView: UIStackView {
     
     func setUI() {
         idLabel.text = "아이디"
+        idTextField.placeholder = "아이디를 입력하세요"
     }
     
     func layout() {
         
         self.addSubview(idLabel)
+        self.addSubview(idTextField)
         idLabel.translatesAutoresizingMaskIntoConstraints = false
+        idTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             idLabel.heightAnchor.constraint(equalToConstant: 22),
             idLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
-            idLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
-
+            idLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            
+            idTextField.leadingAnchor.constraint(equalTo: idLabel.trailingAnchor, constant: 52.74),
+            idTextField.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            idTextField.heightAnchor.constraint(equalToConstant: 22),
         ])
     }
     
