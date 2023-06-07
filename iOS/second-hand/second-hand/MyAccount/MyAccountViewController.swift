73f627684b1c0ff4bd581387a466cbb343d46b39
Haena <ghkwlsspdl@naver.com>
2023-06-07 16:29:25 +0900

    [#9] FEAT: contour세팅
    - 백그라운드 컬러 변경
    - 레이아웃 설정

--- iOS/second-hand/second-hand/MyAccount/MyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/MyAccountViewController.swift
@@ -12,6 +12,7 @@ class MyAccountViewController: UIViewController {
     let idStackView = IdStackView()
     let loginButton = UIButton()
     let joinMembershipButton = UIButton()
+    let contour = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,16 +31,20 @@ class MyAccountViewController: UIViewController {
         
         joinMembershipButton.setTitle("회원가입", for: .normal)
         joinMembershipButton.setTitleColor(.black, for: .normal)
+        
+        contour.backgroundColor = .lightGray
     }
     
     func layout() {
         self.view.addSubview(loginButton)
         self.view.addSubview(idStackView)
         self.view.addSubview(joinMembershipButton)
+        self.view.addSubview(contour)
         
         idStackView.translatesAutoresizingMaskIntoConstraints = false
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         joinMembershipButton.translatesAutoresizingMaskIntoConstraints = false
+        contour.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             idStackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 178),
@@ -54,6 +59,12 @@ class MyAccountViewController: UIViewController {
             joinMembershipButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 19),
             joinMembershipButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             joinMembershipButton.heightAnchor.constraint(equalToConstant: 22),
+            
+            contour.topAnchor.constraint(equalTo: idStackView.bottomAnchor, constant: 0),
+            contour.heightAnchor.constraint(equalToConstant: 0.5),
+            contour.widthAnchor.constraint(equalToConstant: self.view.frame.width)
         ])
     }
+    
+    
 }
