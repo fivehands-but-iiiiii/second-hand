82dcaa5d1752595ef63c5e231e7a45f1181fc6b6
Haena <ghkwlsspdl@naver.com>
2023-06-07 16:18:34 +0900

    [#09] FEAT: idLabel세팅
    - idLabel 세팅
    - idLabel 레이아웃

--- iOS/second-hand/second-hand/MyAccount/IdStackView.swift
+++ iOS/second-hand/second-hand/MyAccount/IdStackView.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class IdStackView: UIStackView {
     let idLabel = UILabel()
-    let idTextField = UITextField()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUI()
+        layout()
     }
     
     required init(coder: NSCoder) {
         fatalError("필수생성자")
     }
+    
+    func setUI() {
+        idLabel.text = "아이디"
+    }
+    
+    func layout() {
+        
+        self.addSubview(idLabel)
+        idLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            idLabel.heightAnchor.constraint(equalToConstant: 22),
+            idLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            idLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+
+        ])
+    }
+    
 }
