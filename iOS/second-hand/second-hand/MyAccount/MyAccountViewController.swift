92db16be14f4a49ce287ea62d79d4b43e00bdf81
Haena <ghkwlsspdl@naver.com>
2023-06-07 16:23:38 +0900

    [#9] FEAT: idStackView를 MyAccountViewController에 띄우기
    - idStackView레이아웃
    - 네비 타이틀을 내 계정으로 지정

--- iOS/second-hand/second-hand/MyAccount/MyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/MyAccountViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class MyAccountViewController: UIViewController {
-
+    
+    let idStackView = IdStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        self.navigationItem.title = "내 계정"
+        layout()
+    }
+    
+    func layout() {
+        self.view.addSubview(idStackView)
+
+        idStackView.translatesAutoresizingMaskIntoConstraints = false
+       
+        NSLayoutConstraint.activate([
+            idStackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 178),
+            idStackView.heightAnchor.constraint(equalToConstant: 44),
+            idStackView.widthAnchor.constraint(equalToConstant: self.view.frame.width),
+        ])
     }
 }
