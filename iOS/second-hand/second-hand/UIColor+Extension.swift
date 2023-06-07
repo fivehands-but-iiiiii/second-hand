812c4dcd2b04d4a2da1cb1ae4a471a03dd307e09
Haena <ghkwlsspdl@naver.com>
2023-06-07 17:01:41 +0900

    [#8] FEAT: UIColor 확장
    - 요구사항에 해당하는 컬러를 UIColor확장을 통하여 쉽게 사용할 수 있도록 구현

---
+++ iOS/second-hand/second-hand/UIColor+Extension.swift
@@ -0,0 +1,123 @@
+//
+//  Extension+UIColor.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/06/07.
+//
+
+import UIKit
+
+extension UIColor {
+    static let white = UIColor(
+        red: 255/255,
+        green: 255/255,
+        blue: 255/255,
+        alpha: 1.0
+    )
+    
+    static let gray50 = UIColor(
+        red: 250/255,
+        green: 250/255,
+        blue: 250/255,
+        alpha: 1.0
+    )
+    
+    static let gray100 = UIColor(
+        red: 249/255,
+        green: 249/255,
+        blue: 249/255,
+        alpha: 0.8
+    )
+    
+    static let gray200 = UIColor(
+        red: 245/255,
+        green: 245/255,
+        blue: 245/255,
+        alpha: 0.7
+    )
+    
+    static let gray300 = UIColor(
+        red: 179/255,
+        green: 179/255,
+        blue: 179/255,
+        alpha: 0.12
+    )
+    
+    static let gray400 = UIColor(
+        red: 118/255,
+        green: 118/255,
+        blue: 128/255,
+        alpha: 0.12
+    )
+    
+    static let gray500 = UIColor(
+        red: 179/255,
+        green: 179/255,
+        blue: 179/255,
+        alpha: 0.39
+    )
+    
+    static let gray600 = UIColor(
+        red: 0/255,
+        green: 0/255,
+        blue: 0/255,
+        alpha: 0.2
+    )
+    
+    static let gray700 = UIColor(
+        red: 60/255,
+        green: 60/255,
+        blue:67/255,
+        alpha: 0.36
+    )
+    
+    static let gray800 = UIColor(
+        red: 60/255,
+        green: 60/255,
+        blue: 67/255,
+        alpha: 0.6
+    )
+    
+    static let gray900 = UIColor(
+        red: 60/255,
+        green: 60/255,
+        blue: 67/255,
+        alpha: 1
+    )
+    
+    static let black = UIColor(
+        red: 0/255,
+        green: 0/255,
+        blue: 0/255,
+        alpha: 1
+    )
+    
+    static let mint = UIColor(
+        red: 0/255,
+        green: 199/255,
+        blue: 190/255,
+        alpha: 1
+    )
+    
+    static let orange = UIColor(
+        red: 255/255,
+        green: 149/255,
+        blue: 0/255,
+        alpha: 1
+    )
+    
+    static let blue = UIColor(
+        red: 0/255,
+        green: 122/255,
+        blue: 255/255,
+        alpha: 1
+    )
+    
+    static let red = UIColor(
+        red: 255/255,
+        green: 59/255,
+        blue: 48/255,
+        alpha: 1
+    )
+    
+}
