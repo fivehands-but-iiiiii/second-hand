dfd8943621d954be21c1074001cac5ec1be3955f
Haena <ghkwlsspdl@naver.com>
2023-06-07 16:25:35 +0900

    [#9] FEAT: 로그인 버튼 생성
    - 로그인버튼 타이틀 지정
    - 로그인버튼 배경색 오렌지색으로 지정
    - 로그인버튼 모서리 둥글게
    - 로그인버튼 레이아웃

--- iOS/second-hand/second-hand/MyAccount/MyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/MyAccountViewController.swift
@@ -10,23 +10,40 @@ import UIKit
 class MyAccountViewController: UIViewController {
     
     let idStackView = IdStackView()
+    let loginButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationItem.title = "내 계정"
         layout()
+        setting()
+    }
+    
+    func setting() {
+        loginButton.setTitle("로그인", for: .normal)
+        loginButton.backgroundColor = .orange
+        self.view.layoutIfNeeded()
+        loginButton.layer.cornerRadius = loginButton.layer.frame.height/2
+        loginButton.layer.masksToBounds = true
     }
     
     func layout() {
+        self.view.addSubview(loginButton)
         self.view.addSubview(idStackView)
-
+      
         idStackView.translatesAutoresizingMaskIntoConstraints = false
-       
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             idStackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 178),
             idStackView.heightAnchor.constraint(equalToConstant: 44),
             idStackView.widthAnchor.constraint(equalToConstant: self.view.frame.width),
+            
+            loginButton.widthAnchor.constraint(equalToConstant: 361),
+            loginButton.heightAnchor.constraint(equalToConstant: 52),
+            loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            loginButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -204),
         ])
     }
 }
