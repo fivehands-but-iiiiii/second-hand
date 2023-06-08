09152182c894a20845e53c12fa05a6c213602c39
Haena <ghkwlsspdl@naver.com>
2023-06-08 17:58:07 +0900

    [#16] FEAT: 카테고리 라벨을 스크롤링으로 컨트롤
    - 라벨 엣지 변경
    - 전체 라벨 생성하는 메소드 등록
    - 스크롤뷰 좌우 제약

--- iOS/second-hand/second-hand/WishListViewController.swift
+++ iOS/second-hand/second-hand/WishListViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 
 class WishListViewController: UIViewController {
     var categoriScrollView = CategoriesScrollView()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .white
-        self.navigationItem.title = "관심 목록"
-        categoriScrollView.backgroundColor = .purple
-        layout()
-    }
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           self.view.backgroundColor = .white
+           self.navigationItem.title = "관심 목록"
+           layout()
+           setup()
+           makeLabel(categori: "가구/인테리어")
+           makeLabel(categori: "안녕안녕")
+           makeLabel(categori: "하하하")
+           makeLabel(categori: "스크롤~~된다~~")
+           makeLabel(categori: "길게길게길게길게길게길게")
+       }
     
+    func setup() {
+        let allCategoriButton = CategoriButton(title: "전체")
+        categoriScrollView.categoriStackView.addArrangedSubview(allCategoriButton)
+        allCategoriButton.backgroundColor = UIColor.orange
+        allCategoriButton.setTitleColor(.white, for: .normal)
+        allCategoriButton.layer.borderWidth = 0
+    }
+       
     func layout() {
         self.view.addSubview(categoriScrollView)
         categoriScrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -25,7 +38,11 @@ class WishListViewController: UIViewController {
             categoriScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
             categoriScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
             categoriScrollView.heightAnchor.constraint(equalToConstant: 32)
-                ])
-
+        ])
     }
+       
+       func makeLabel(categori: String) {
+           let categoriLabel = CategoriButton(title: categori)
+           categoriScrollView.categoriStackView.addArrangedSubview(categoriLabel)
+       }
 }
