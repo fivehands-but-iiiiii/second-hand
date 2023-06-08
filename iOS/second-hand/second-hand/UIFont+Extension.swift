c8e51b032536bcf9a997e370939725713f8e0555
Haena <ghkwlsspdl@naver.com>
2023-06-09 00:29:18 +0900

    [#27] FIX: 폰트 익스텐션 수정
    - 폰트가 적용되지 않는 버그 수정

--- iOS/second-hand/second-hand/UIFont+Extension.swift
+++ iOS/second-hand/second-hand/UIFont+Extension.swift
@@ -7,21 +7,16 @@
 
 import UIKit
 extension UIFont {
-    enum FontType: String {
-        case semibold = "semibold"
-        case regular = "regular"
-    }
-    
-    static let largeTitle = UIFont(name: FontType.regular.rawValue, size: 34)
-    static let title1 = UIFont(name: FontType.regular.rawValue, size: 28)
-    static let title2 = UIFont(name: FontType.regular.rawValue, size: 22)
-    static let title3 = UIFont(name: FontType.regular.rawValue, size: 20)
-    static let headLine = UIFont(name: FontType.semibold.rawValue, size: 17)
-    static let body = UIFont(name: FontType.regular.rawValue, size: 17)
-    static let callOut = UIFont(name: FontType.regular.rawValue, size: 16)
-    static let subHead = UIFont(name: FontType.regular.rawValue, size: 15)
-    static let footNote = UIFont(name: FontType.regular.rawValue, size: 13)
-    static let caption1 = UIFont(name: FontType.regular.rawValue, size: 12)
-    static let caption2 = UIFont(name: FontType.regular.rawValue, size: 11)
+    static let largeTitle = UIFont.systemFont(ofSize: 34, weight: .regular)
+    static let title1 = UIFont.systemFont(ofSize: 28, weight: .regular)
+    static let title2 = UIFont.systemFont(ofSize: 22, weight: .regular)
+    static let title3 = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let headLine = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    static let body = UIFont.systemFont(ofSize: 17, weight: .regular)
+    static let callOut = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let subHead = UIFont.systemFont(ofSize: 15, weight: .regular)
+    static let footNote = UIFont.systemFont(ofSize: 13, weight: .regular)
+    static let caption1 = UIFont.systemFont(ofSize: 12, weight: .regular)
+    static let caption2 = UIFont.systemFont(ofSize: 11, weight: .regular)
 }
 
