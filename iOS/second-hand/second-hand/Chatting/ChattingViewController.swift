7b69636280ab8988ba35e5aef97e14cfbf83dcef
Haena <ghkwlsspdl@naver.com>
2023-06-09 01:27:48 +0900

    [#27] DESIGN: 네비게이션바 하단 라인 추가
    - 모든 탭의 뷰컨트롤러에 네비게이션 하단라인 추가
    - 추후 뷰 컨트롤러를 네비게이션하단라인을 추가해 커스텀하여 상속시킬 예정

--- iOS/second-hand/second-hand/Chatting/ChattingViewController.swift
+++ iOS/second-hand/second-hand/Chatting/ChattingViewController.swift
@@ -15,6 +15,12 @@ class ChattingViewController: UIViewController {
         let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .bold)]
         self.navigationController?.navigationBar.titleTextAttributes = attributes
         self.navigationItem.title = "채팅"
-        
+        setNavigationBarBottomBorder()
     }
+    
+    func setNavigationBarBottomBorder() {
+            let borderView = UIView(frame: CGRect(x: 0, y: navigationController?.navigationBar.frame.height ?? 0 - 1, width: navigationController?.navigationBar.frame.width ?? 0, height: 1))
+            borderView.backgroundColor = .lightGray
+            navigationController?.navigationBar.addSubview(borderView)
+        }
 }
--- iOS/second-hand/second-hand/SaleLog/Controller/SaleLogViewController.swift
+++ iOS/second-hand/second-hand/SaleLog/Controller/SaleLogViewController.swift
@@ -12,5 +12,12 @@ class SaleLogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        setNavigationBarBottomBorder()
     }
+    
+    func setNavigationBarBottomBorder() {
+            let borderView = UIView(frame: CGRect(x: 0, y: navigationController?.navigationBar.frame.height ?? 0 - 1, width: navigationController?.navigationBar.frame.width ?? 0, height: 1))
+            borderView.backgroundColor = .lightGray
+            navigationController?.navigationBar.addSubview(borderView)
+        }
 }
