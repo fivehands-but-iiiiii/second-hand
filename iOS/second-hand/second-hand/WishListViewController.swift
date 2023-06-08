39d8bbaee03a96b0c2a9967ce346afa06a2e16d9
Haena <ghkwlsspdl@naver.com>
2023-06-08 15:55:22 +0900

    [#16] FEAT: 위시리스트 스크롤뷰 레이아웃
    - 위시리스트 스크롤뷰 레이아웃
    - 커스텀 스크롤뷰 구현
    - 커스텀 라벨 구현
    - 커스텀 스택뷰 구현

--- iOS/second-hand/second-hand/WishListViewController.swift
+++ iOS/second-hand/second-hand/WishListViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class WishListViewController: UIViewController {
-
+    var categoriScrollView = CategoriesScrollView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationItem.title = "관심 목록"
+        categoriScrollView.backgroundColor = .purple
+        layout()
+    }
+    
+    func layout() {
+        self.view.addSubview(categoriScrollView)
+        categoriScrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            categoriScrollView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 114),
+            categoriScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            categoriScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            categoriScrollView.heightAnchor.constraint(equalToConstant: 32)
+                ])
+
     }
 }
