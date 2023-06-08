39add380ddfecfd5f846cc7bc49169bd8dd36a4d
Haena <ghkwlsspdl@naver.com>
2023-06-09 03:20:48 +0900

    [#29] FEAT: 세일로그UI세팅
    - 네비게이션바의 뷰를 커스텀
    - 레이블세팅
    - 세그먼트컨트롤 세팅
    - 네비게이션바 높이 설정

--- iOS/second-hand/second-hand/SaleLog/Controller/SaleLogViewController.swift
+++ iOS/second-hand/second-hand/SaleLog/Controller/SaleLogViewController.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class SaleLogViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
         setNavigationBarBottomBorder()
+        setupNavigationBarHeight()
+        navigationItem.titleView = SaleLogNaviView()
     }
     
     func setNavigationBarBottomBorder() {
-            let borderView = UIView(frame: CGRect(x: 0, y: navigationController?.navigationBar.frame.height ?? 0 - 1, width: navigationController?.navigationBar.frame.width ?? 0, height: 1))
-            borderView.backgroundColor = .lightGray
-            navigationController?.navigationBar.addSubview(borderView)
+        let borderView = UIView(frame: CGRect(x: 0, y: navigationController?.navigationBar.frame.height ?? 0 - 1, width: navigationController?.navigationBar.frame.width ?? 0, height: 1))
+        borderView.backgroundColor = .lightGray
+        navigationController?.navigationBar.addSubview(borderView)
+    }
+    
+    func setupNavigationBarHeight() {
+        if let navigationController = self.navigationController {
+            let navigationBarHeight: CGFloat = 100 // 원하는 네비게이션 바 높이
+            let statusBarHeight = UIApplication.shared.statusBarFrame.height
+            let newNavigationBarFrame = CGRect(x: 0, y: 0, width: view.frame.width, height: navigationBarHeight + statusBarHeight)
+            navigationController.navigationBar.frame = newNavigationBarFrame
+            navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navigationController.navigationBar.shadowImage = UIImage()
+            navigationController.navigationBar.isTranslucent = true
+            
+            let titleView = SaleLogNaviView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: navigationBarHeight))
+            navigationItem.titleView = titleView
         }
+    }
+    
+    func setUI() {
+
+    }
 }
+
