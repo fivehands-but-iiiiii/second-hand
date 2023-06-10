3194f762b9e4637e33d1635048cff2fe2815f1b4
SONG <geun2121@gmail.com>
2023-06-10 20:45:14 +0900

    [#38] CHORE: 가독성 향상

--- iOS/second-hand/second-hand/SaleLog/Controller/SaleLogViewController.swift
+++ iOS/second-hand/second-hand/SaleLog/Controller/SaleLogViewController.swift
@@ -12,36 +12,32 @@ class SaleLogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
+        
+        setNavigationBarFrame()
+        setTitleView()
         setNavigationBarBottomBorder()
-        setupNavigationBarHeight()
+    }
+    
+    private func setNavigationBarFrame() {
+        navigationController?.navigationBar.frame = CGRect(origin: .zero, size: CGSize(width: view.frame.width, height: 100))
     }
     
     func setNavigationBarBottomBorder() {
-        let borderView = UIView(frame: CGRect(x: 0, y: navigationController?.navigationBar.frame.height ?? 0 , width: navigationController?.navigationBar.frame.width ?? 0, height: 1))
-        borderView.backgroundColor = .lightGray
-        navigationController?.navigationBar.addSubview(borderView)
-    }
-    
-    func setupNavigationBarHeight() {
-        if let navigationController = self.navigationController {
-            let navigationBarHeight: CGFloat = 100
-            let statusBarHeight = UIApplication.shared.statusBarFrame.height
-            let newNavigationBarFrame = CGRect(x: 0, y: 0, width: view.frame.width, height: navigationBarHeight + statusBarHeight)
-            navigationController.navigationBar.frame = newNavigationBarFrame
-            
-            navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
-            navigationController.navigationBar.shadowImage = UIImage()
-            navigationController.navigationBar.isTranslucent = true
-            
-            let titleView = SaleLogNavigationView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 300))
-            navigationController.navigationBar.topItem?.titleView = titleView
-            
+        guard let navigationBar = navigationController?.navigationBar else {
+            return
         }
-    }
-    
-    func setUI() {
         
+        let borderView = UIView(frame: CGRect(x: .zero, y: navigationBar.frame.maxY, width: navigationBar.frame.width, height: 1))
+        
+        borderView.backgroundColor = .lightGray
+        navigationBar.addSubview(borderView)
     }
+
+    func setTitleView() {
+        let titleView = SaleLogNavigationView(frame: CGRect(x: .zero, y: .zero, width: view.frame.width, height: 300))
+        
+        navigationController?.navigationBar.topItem?.titleView = titleView
+    }
+
 }
 
