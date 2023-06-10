8f4ff39459f215586cec1c83fa76b2b77dd0971d
SONG <geun2121@gmail.com>
2023-06-10 18:18:45 +0900

    [#38] ADD: TODO주석 추가
    - 딱봤을때 필요하겠다 싶은 부분 주석으로 작성

--- iOS/second-hand/second-hand/CategoryButton.swift
+++ iOS/second-hand/second-hand/CategoryButton.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class CategoriButton: UIButton {
-    
+class CategoryButton: UIButton {
+    // TODO: 이 클래스 손 좀 봐야겠음
     convenience init(title: String) {
         self.init()
         self.setTitle(title, for: .normal)
@@ -18,6 +18,7 @@ class CategoriButton: UIButton {
         self.layer.masksToBounds = true
         self.layer.cornerRadius = self.bounds.height / 2
         self.contentEdgeInsets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        // TODO: 이거 디플리케잍 된거. 바꿔야함
         self.setTitleColor(.black, for: .normal)
         self.titleLabel?.font = UIFont.caption1
     }
@@ -34,4 +35,5 @@ class CategoriButton: UIButton {
         super.layoutSubviews()
         self.layer.cornerRadius = self.bounds.height / 2
     }
+    // TODO: layoutSubView에서 하는방법 말고 다른 방법 강구
 }
