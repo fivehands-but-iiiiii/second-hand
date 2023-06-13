f12a1531b81e67a4977f4236041de1a36b9efa2e
Haena <ghkwlsspdl@naver.com>
2023-06-13 11:30:58 +0900

    [#56] FEAT: loginViewController 생성
    - 내계정 탭의 로그인 버튼을 누를시 loginViewController로 이동하는 로직 설정

--- iOS/second-hand/second-hand/MyAccount/Controller/MyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/Controller/MyAccountViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class MyAccountViewController: NavigationUnderLineViewController {
+    var loginViewController = LoginViewController()
     
     let idStackView = IdStackView()
     let loginButton = UIButton()
@@ -34,6 +35,11 @@ class MyAccountViewController: NavigationUnderLineViewController {
         self.view.layoutIfNeeded()
         loginButton.layer.cornerRadius = loginButton.layer.frame.height/2
         loginButton.layer.masksToBounds = true
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func loginButtonTapped() {
+        present(loginViewController, animated: true)
     }
     
     func setJoinMembershipButton() {
