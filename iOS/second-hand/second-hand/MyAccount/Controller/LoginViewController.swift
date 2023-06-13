61c94142c517e1a1d174bc8ec3ee86e99d9b8b8f
Haena <ghkwlsspdl@naver.com>
2023-06-13 11:30:58 +0900

    [#56] FEAT: loginViewController 생성
    - 내계정 탭의 로그인 버튼을 누를시 loginViewController로 이동하는 로직 설정

---
+++ iOS/second-hand/second-hand/MyAccount/Controller/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/06/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+    
+
+}
