fc699ca5d81b9aa1e43bd3c369fa66525767c5da
SONG <geun2121@gmail.com>
2023-06-14 10:54:54 +0900

    [#60] FEAT: Observer 설치
    - "LOGIN" notification을 감시하는 옵저버

--- iOS/second-hand/second-hand/Home/Controller/HomeViewController.swift
+++ iOS/second-hand/second-hand/Home/Controller/HomeViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class HomeViewController: NavigationUnderLineViewController {
+    private var isLogin = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setObserver()
+    }
+    
+    
+    private func setObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didRecieveLogin(_:)), name: NSNotification.Name("LOGIN"), object: nil)
+    }
+    
+    @objc func didRecieveLogin(_ notification: Notification) {
+        self.isLogin = true
+        print("로그인 되었습니다.")
     }
     
     private func setUI() {
