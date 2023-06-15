aba15e0be49677ea9d35f7410bcd5a96017011f8
Haena <ghkwlsspdl@naver.com>
2023-06-15 17:08:55 +0900

    [#77] FEAT: 로그인 된 내계정 화면 임시세팅
    - 로그인버튼을 눌러 로그인이 된 상태로 만든 후 탭바의 내계정을 들어간다면 로그인 된 내 계정 화면이 나오는것을 쉽게 확인하기 위해 라벨 세팅

--- iOS/second-hand/second-hand/MyAccount/Controller/LoginMyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/Controller/LoginMyAccountViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class LoginMyAccountViewController: NavigationUnderLineViewController {
+    let label = UILabel(frame: CGRect(x: 300, y: 300, width: 50, height: 50))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "로그인이 된 내계정 화면임 !!"
         
+        self.view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            ])
+            
     }
     
 
