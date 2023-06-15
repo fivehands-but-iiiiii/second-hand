79d2660051da13131a7d29218109dcde46f884b6
Haena <ghkwlsspdl@naver.com>
2023-06-16 00:50:06 +0900

    [#92] REFACTOR: 위시리스트 로직 변경
    - 메서드를 통합시킴

--- iOS/second-hand/second-hand/WishListViewController.swift
+++ iOS/second-hand/second-hand/WishListViewController.swift
@@ -8,43 +8,41 @@
 import UIKit
 
 class WishListViewController: NavigationUnderLineViewController {
-    var categoriScrollView = CategoriesScrollView()
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           layout()
-           setup()
-           makeButton(category: "가구/인테리어")
-           makeButton(category: "안녕안녕")
-           makeButton(category: "하하하")
-           makeButton(category: "스크롤~~된다~~")
-           makeButton(category: "길게길게길게길게길게길게")
-       }
+    var categoryScrollView = CategoriesScrollView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        makeButton(category: "가구/인테리어")
+        makeButton(category: "안녕안녕")
+        makeButton(category: "하하하")
+        makeButton(category: "스크롤~~된다~~")
+        makeButton(category: "길게길게길게길게길게길게")
+    }
     
-    func setup() {
+    func setupUI() {
         //네비게이션
-        
         self.navigationItem.title = "관심 목록"
-        
         let allCategoriButton = CategoryButton(title: "전체")
-        categoriScrollView.categoriStackView.addArrangedSubview(allCategoriButton)
+        categoryScrollView.categoriStackView.addArrangedSubview(allCategoriButton)
         allCategoriButton.backgroundColor = UIColor.orange
         allCategoriButton.setTitleColor(UIColor.white, for: .normal)
         allCategoriButton.layer.borderWidth = 0
+        categoryLayout()
     }
-       
-    func layout() {
-        self.view.addSubview(categoriScrollView)
-        categoriScrollView.translatesAutoresizingMaskIntoConstraints = false
+    
+    func categoryLayout() {
+        self.view.addSubview(categoryScrollView)
+        categoryScrollView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            categoriScrollView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 114),
-            categoriScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-            categoriScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
-            categoriScrollView.heightAnchor.constraint(equalToConstant: 32)
+            categoryScrollView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 114),
+            categoryScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            categoryScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            categoryScrollView.heightAnchor.constraint(equalToConstant: 32)
         ])
     }
-       
-       func makeButton(category: String) {
-           let categoriLabel = CategoryButton(title: category)
-           categoriScrollView.categoriStackView.addArrangedSubview(categoriLabel)
-       }
+    
+    func makeButton(category: String) {
+        let categoryLabel = CategoryButton(title: category)
+        categoryScrollView.categoriStackView.addArrangedSubview(categoryLabel)
+    }
 }
