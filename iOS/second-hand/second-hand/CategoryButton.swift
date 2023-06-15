85498c75c135b53965d967e0eb3743b3925a6827
Haena <ghkwlsspdl@naver.com>
2023-06-16 02:41:04 +0900

    [#92] DOCS: 주석 추가
    - contentEdgeInsets을 대체할 무언가를 리뷰어에게 여쭤보기위한 주석추가

--- iOS/second-hand/second-hand/CategoryButton.swift
+++ iOS/second-hand/second-hand/CategoryButton.swift
@@ -25,6 +25,7 @@ final class CategoryButton: UIButton {
         self.layer.borderWidth = 1
         self.layer.masksToBounds = true
         self.layer.cornerRadius = self.bounds.height / 2
+        //contentEdgeInsets를 어떤걸로 대체할 수 있을까요?
         self.contentEdgeInsets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
         self.setTitleColor(.black, for: .normal)
         self.titleLabel?.font = UIFont.caption1
