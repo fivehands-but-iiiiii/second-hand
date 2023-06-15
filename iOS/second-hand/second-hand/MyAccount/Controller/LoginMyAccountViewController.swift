be065982a6c7b26093e4e88d9b35c552c27e6dfa
Haena <ghkwlsspdl@naver.com>
2023-06-15 17:21:34 +0900

    [#77] REFACTOR: setNotLoginUI메서드 위치 변경
    - 가독성을 높이기 위해 setLoginedUI뒤에 배치
    - 중구난방이였던 addTarget메서드들 제자리 찾아줌

--- iOS/second-hand/second-hand/MyAccount/Controller/LoginMyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/Controller/LoginMyAccountViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class LoginMyAccountViewController: NavigationUnderLineViewController {
     let label = UILabel(frame: CGRect(x: 300, y: 300, width: 50, height: 50))
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = "로그인이 된 내계정 화면임 !!"
@@ -19,9 +19,6 @@ class LoginMyAccountViewController: NavigationUnderLineViewController {
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-            ])
-            
+        ])
     }
-    
-
 }
