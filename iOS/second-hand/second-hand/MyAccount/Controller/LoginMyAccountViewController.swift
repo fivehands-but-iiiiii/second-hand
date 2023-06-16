abe95bad94ddb87cc721e6c8d6df5d91e4494333
Haena <ghkwlsspdl@naver.com>
2023-06-16 11:31:27 +0900

    [#96] DESIGN: 프로필사진이 올라갈 동그라미 버튼세팅
    - 세팅

--- iOS/second-hand/second-hand/MyAccount/Controller/LoginMyAccountViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/Controller/LoginMyAccountViewController.swift
@@ -7,18 +7,49 @@
 
 import UIKit
 
-class LoginMyAccountViewController: NavigationUnderLineViewController {
-    let label = UILabel(frame: CGRect(x: 300, y: 300, width: 50, height: 50))
+final class LoginMyAccountViewController: NavigationUnderLineViewController {
+    private let circleButton = UIButton()
+    private let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCircleButton()
+        setLabel()
+        layout()
+    }
+    
+    private func setLabel() {
+        label.frame = CGRect(x: 300, y: 300, width: 50, height: 50)
         label.text = "로그인이 된 내계정 화면임 !!"
+    }
+    
+    private func setCircleButton() {
+        circleButton.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        circleButton.layer.cornerRadius =  circleButton.layer.frame.size.width/2
+        circleButton.layer.masksToBounds = true
+        circleButton.layer.borderWidth = 1
+        circleButton.layer.borderColor = CGColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1)
+        circleButton.setImage(UIImage(systemName: "camera"), for: .normal)
+        circleButton.tintColor = .white
+        circleButton.backgroundColor = .black
+    }
+    
+    private func layout() {
+        self.view.addSubview(circleButton)
+        circleButton.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
+        let height: CGFloat = self.view.frame.height
+        let width: CGFloat = self.view.frame.width
+        let figmaHeight: CGFloat = 852
+        let figmaWidth: CGFloat = 393
+        let heightRatio: CGFloat = height/figmaHeight
+        let widthRatio: CGFloat = width/figmaWidth
+        
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            circleButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            circleButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 194*heightRatio),
+            circleButton.widthAnchor.constraint(equalToConstant: 80),
+            circleButton.heightAnchor.constraint(equalToConstant: 80),
         ])
     }
 }
