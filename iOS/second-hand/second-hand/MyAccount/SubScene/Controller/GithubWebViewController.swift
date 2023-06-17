666a287d8c96ff5c5c102b2b15e849c134250e71
SONG <geun2121@gmail.com>
2023-06-18 01:46:12 +0900

    [#76] FEAT: webView 추가
    - import WebKit 
    - KNavigationDelegate 구현

--- iOS/second-hand/second-hand/MyAccount/SubScene/Controller/GithubWebViewController.swift
+++ iOS/second-hand/second-hand/MyAccount/SubScene/Controller/GithubWebViewController.swift
@@ -6,20 +6,24 @@
 //
 
 import UIKit
+import WebKit
 
 class GithubWebViewController: UIViewController {
     
     var webView = UIWebView()
+    private var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        layout()
+        setupWebView()
         webViewLogic()
     }
     
-    func layout() {
-        view.addSubview(webView)
+    private func setupWebView() {
+        webView = WKWebView(frame: view.bounds)
+        webView.navigationDelegate = self
         webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
         
         NSLayoutConstraint.activate([
         webView.topAnchor.constraint(equalTo: self.view.topAnchor),
@@ -33,5 +37,12 @@ class GithubWebViewController: UIViewController {
         let url = URL(string: "https://github.com/login/oauth/authorize?client_id=aeceaccd71d24266b1d3&redirect_url=http://localhost:8080/login/oauth2/code/github")
         let request = URLRequest(url: url!)
         webView.loadRequest(request)
+extension GithubWebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url {
+            handleRedirectURL(url: url)
+        }
+        decisionHandler(.allow)
     }
 }
