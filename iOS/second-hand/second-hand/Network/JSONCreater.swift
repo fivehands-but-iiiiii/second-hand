07ef71a77d5edb2d9e0d3e56e514e4064f004d33
SONG <geun2121@gmail.com>
2023-06-18 02:11:33 +0900

    [#76]  FEAT: "region" 정보를 포함한 JSON
    - 회원가입이 필요한 유저는 동네설정에 대한 정보를 추가로 붙여서 POST해야함

---
+++ iOS/second-hand/second-hand/Network/JSONCreater.swift
@@ -0,0 +1,25 @@
+//
+//  JSONCreater.swift
+//  second-hand
+//
+//  Created by SONG on 2023/06/17.
+//
+
+import Foundation
+
+class JSONCreater {
+    func createJSON(user: GitUserNeedsJoin, region: Region) -> Data? {
+        
+        let userData = GitUserNeedsJoinRegisterRequest(memberId: user.login, profileImgURL: user.avatar_url, regions: [region])
+
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(userData)
+            print(String(data: jsonData, encoding: .utf8))
+            return jsonData
+        } catch {
+            print("JSON encoding error: \(error)")
+            return nil
+        }
+    }
+}
