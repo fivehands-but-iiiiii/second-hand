627090777fa2841d8b26e5d7878d3a87914c8116
Haena <ghkwlsspdl@naver.com>
2023-06-18 20:52:14 +0900

    [#100] ADD: 첫번째 동네설정 버튼 추가
    - 첫번쨰 동네설정 버튼 세팅
    - 레이아웃
    - 주석추가

--- iOS/second-hand/second-hand/Home/SetLocationViewController.swift
+++ iOS/second-hand/second-hand/Home/SetLocationViewController.swift
@@ -45,16 +45,36 @@ final class SetLocationViewController: NavigationUnderLineViewController {
         commentLabel.numberOfLines = 2
         commentLabel.textAlignment = .center
     }
-    
+    //TODO: 버튼 이미지 사이즈 변경하는것 찾아보기
+    //TODO: 이거 버튼 위에 레이블을 올려야 할것같음
+    //이유: 텍스트가 늘어나는 길이에 맞게 유동적으로 스페이싱이 변해야할것같아서
     private func setFirstLocation() {
+        var configuration = UIButton.Configuration.tinted()
+        configuration.image = UIImage(systemName: "xmark")
+        configuration.imagePlacement = .trailing
+        configuration.title = "역삼1동"
+        configuration.imagePadding = 50
+        firstLocation.configuration = configuration
         
-        
+        firstLocation.tintColor = .accentText
+        firstLocation.setTitleColor(.accentText, for: .normal)
+        firstLocation.titleLabel?.font = .subHead
+        firstLocation.backgroundColor = .accentBackgroundPrimary
+        firstLocation.layer.cornerRadius = 8
+        firstLocation.layer.masksToBounds = true
     }
     
+    //TODO: 버튼 이미지 사이즈 변경하는것 찾아보기
     private func setSecondLocation() {
-        secondLocation.setImage(UIImage(systemName: "plus"), for: .normal)
+        var configuration = UIButton.Configuration.tinted()
+        configuration.image = UIImage(systemName: "plus")
+        configuration.imagePlacement = .leading
+        configuration.title = "label"
+        configuration.imagePadding = 4
+        configuration.baseBackgroundColor = .white
+        secondLocation.configuration = configuration
+        
         secondLocation.tintColor = UIColor.accentTextWeak
-        secondLocation.setTitle("label", for: .normal)
         secondLocation.setTitleColor(.accentTextWeak, for: .normal)
         secondLocation.titleLabel?.font = UIFont.subHead
         secondLocation.layer.borderColor = CGColor(red: 179/255, green: 179/255, blue: 179/255, alpha: 0.39)
@@ -65,10 +85,10 @@ final class SetLocationViewController: NavigationUnderLineViewController {
     }
     
     private func layout() {
-        self.view.addSubview(commentLabel)
-        self.view.addSubview(secondLocation)
-        commentLabel.translatesAutoresizingMaskIntoConstraints = false
-        secondLocation.translatesAutoresizingMaskIntoConstraints = false
+        [commentLabel, firstLocation, secondLocation].forEach{
+            self.view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
         
         let height: CGFloat = self.view.frame.height
         let width: CGFloat = self.view.frame.width
@@ -80,6 +100,11 @@ final class SetLocationViewController: NavigationUnderLineViewController {
         NSLayoutConstraint.activate([
             commentLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             commentLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 96*heightRatio),
+            
+            firstLocation.topAnchor.constraint(equalTo: commentLabel.bottomAnchor, constant: 48*heightRatio),
+            firstLocation.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 15*widthRatio),
+            firstLocation.widthAnchor.constraint(equalToConstant: 177.5*widthRatio),
+            firstLocation.heightAnchor.constraint(equalToConstant: 52*heightRatio),
             
             secondLocation.topAnchor.constraint(equalTo: commentLabel.bottomAnchor, constant: 48*heightRatio),
             secondLocation.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -15*widthRatio),
