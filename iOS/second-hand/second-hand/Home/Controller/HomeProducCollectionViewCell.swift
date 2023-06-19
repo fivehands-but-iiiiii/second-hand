e051ba5b5afc6f10a391598999bbd8bc2fa4aaba
Haena <ghkwlsspdl@naver.com>
2023-06-20 06:43:58 +0900

    [#130] ADD: 컬렉션뷰셀에 필요한 변수 선언
    - 필요한 변수 선언

--- iOS/second-hand/second-hand/Home/Controller/HomeProducCollectionViewCell.swift
+++ iOS/second-hand/second-hand/Home/Controller/HomeProducCollectionViewCell.swift
@@ -8,5 +8,26 @@
 import UIKit
 
 class HomeProducCollectionViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    let title = UILabel()
+    let location = UILabel()
+    let dot = UILabel()
+    let registerTime = UILabel()
+    let statusLabel = UILabel()
+    let price = UILabel()
+    let chat = UILabel()
+    let wish = UILabel()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        
+    }
 }
