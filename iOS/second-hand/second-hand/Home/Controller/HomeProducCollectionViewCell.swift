091976097dfab585cd83a03fb2801db353632637
Haena <ghkwlsspdl@naver.com>
2023-06-20 06:36:03 +0900

    [#130] ADD: 홈프로덕트리스트 컬렉션뷰셀 생성

---
+++ iOS/second-hand/second-hand/Home/Controller/HomeProducCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeProducCollectionViewCell.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/06/20.
+//
+
+import UIKit
+
+class HomeProducCollectionViewCell: UICollectionViewCell {
+    
+}
