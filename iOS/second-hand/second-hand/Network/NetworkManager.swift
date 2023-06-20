4c75e725e2ac216108e27a5888dc6545ca7d9c82
SONG <geun2121@gmail.com>
2023-06-20 20:26:51 +0900

    [#76] FEAT: header 디코딩 기능 추가
    - header의 cookie값을 디코딩하여 post
    - header에서 cookie값 추출 기능 추가

--- iOS/second-hand/second-hand/Network/NetworkManager.swift
+++ iOS/second-hand/second-hand/Network/NetworkManager.swift
@@ -11,9 +11,9 @@ import OSLog
 class NetworkManager {
     let logger = Logger()
     
-    func requestGET(fromURL url: URL, completion: @escaping (Result<Codable, Error>) -> Void) {
+    func requestGET(fromURL url: URL, completion: @escaping (Result<[Codable], Error>) -> Void) {
         
-        let asyncCompletion: (Result<Codable, Error>) -> Void = { result in
+        let asyncCompletion: (Result<[Codable], Error>) -> Void = { result in
             DispatchQueue.main.async {
                 completion(result)
             }
@@ -23,19 +23,28 @@ class NetworkManager {
         request.httpMethod = HttpMethod.get.method
         
         let session = URLSession.shared.dataTask(with: request) { data, response, error in
+            
             do {
+                guard let data = data else {
+                    return
+                }
                 
-                print(String(data: data!, encoding: .utf8))
                 guard let urlResponse = response as? HTTPURLResponse else {
                     return asyncCompletion(.failure(ManagerErrors.invalidResponse))
                 }
+                
                 switch urlResponse.statusCode {
                 case 401 :
-                    let responseData = try JSONDecoder().decode(GitUserJoinDTO.self, from: data!)
-                    asyncCompletion(.success(responseData.body))
+                    let responseData = try JSONDecoder().decode(GitUserJoinDTO.self, from: data)
+                    
+                    guard let header = self.decodeHeader(httpResponse: urlResponse) else {
+                        return
+                    }
+                    
+                    asyncCompletion(.success([header,responseData.body]))
                 case 200..<300 :
-                    let responseData = try JSONDecoder().decode(GitUserLoginDTO.self, from: data!)
-                    asyncCompletion(.success(responseData.body))
+                    let responseData = try JSONDecoder().decode(GitUserLoginDTO.self, from: data)
+                    asyncCompletion(.success([responseData.data]))
                 default :
                     return asyncCompletion(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
                 }
@@ -46,31 +55,58 @@ class NetworkManager {
         session.resume()
     }
     
-    func requestPOST(data: Data?, fromURL url: URL, completion: @escaping (Result<Codable, Error>) -> Void) {
-            
-            let asyncCompletion: (Result<Codable, Error>) -> Void = { result in
-                DispatchQueue.main.async {
-                    completion(result)
-                }
+    func requestPOST(data: Data?, header: ResponseHeader, fromURL url: URL, completion: @escaping (Result<Codable, Error>) -> Void) {
+        
+        let asyncCompletion: (Result<Codable, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
             }
-            
-            var request = URLRequest(url: url, timeoutInterval: 10.0)
-            request.httpMethod = HttpMethod.post.method
-            request.allHTTPHeaderFields = ["Content-Type": "application/json"]
-            request.httpBody = data
-            
-            let session = URLSession.shared.dataTask(with: request) { data, response, error in
-                    guard let urlResponse = response as? HTTPURLResponse else {
-                        return asyncCompletion(.failure(ManagerErrors.invalidResponse))
-                    }
-                    switch urlResponse.statusCode {
-                    case 200..<300 :
-                        print(String(data: data!, encoding: .utf8))
-                    default :
-                        return asyncCompletion(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
-                    }
-            }
-            session.resume()
         }
+        
+        let keyOfSetCookie : String = ResponseHeader.CodingKeys.setCookie.rawValue
+        let valueOfSetCookie : String = header.setCookie.description
+
+        var request = URLRequest(url: url, timeoutInterval: 10.0)
+        request.httpMethod = HttpMethod.post.method
+        request.allHTTPHeaderFields = ["Cookie" : valueOfSetCookie,"Content-Type":"application/json"]
+        request.httpBody = data
+        
+        let session = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let urlResponse = response as? HTTPURLResponse else {
+                return asyncCompletion(.failure(ManagerErrors.invalidResponse))
+            }
+            print(urlResponse.statusCode)
+            switch urlResponse.statusCode {
+            case 200..<300 :
+            print("POST 성공 ",String(data: data!, encoding: .utf8))
+                return asyncCompletion(.success(data))
+            default :
+                return asyncCompletion(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
+            }
+        }
+        session.resume()
+    }
+    
+    private func decodeHeader(httpResponse : HTTPURLResponse) -> Codable? {
+        do {
+            let setCookieHeaders = httpResponse.allHeaderFields.filter { key, _ in
+                if let headerKey = key as? String {
+                    return headerKey.lowercased() == "set-cookie"
+                }
+                return false
+            }
+            
+            let jsonData = try JSONSerialization.data(withJSONObject: setCookieHeaders, options: [])
+            
+            // JSON decoding
+            let decoder = JSONDecoder()
+            let responseHeader = try decoder.decode(ResponseHeader.self, from: jsonData)
+            //print(responseHeader.setCookie)
+            return responseHeader
+        } catch {
+            print("JSON 디코딩 에러: \(error)")
+            return nil
+        }
+    }
 }
 
