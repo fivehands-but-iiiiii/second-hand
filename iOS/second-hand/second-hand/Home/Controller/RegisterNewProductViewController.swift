cbf8531b9423ed18574d3fb8cf3f276f6003c8cc
Haena <ghkwlsspdl@naver.com>
2023-06-21 17:05:47 +0900

    [#146] FEAT: 레지스터버튼 누를시 상품등록화면으로 이동
    - 화면이동 구현
    - 상품등록뷰컨트롤러 생성

---
+++ iOS/second-hand/second-hand/Home/Controller/RegisterNewProductViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterNewProductViewController.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/06/21.
+//
+
+import UIKit
+
+class RegisterNewProductViewController: NavigationUnderLineViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+        
+    }
+}
