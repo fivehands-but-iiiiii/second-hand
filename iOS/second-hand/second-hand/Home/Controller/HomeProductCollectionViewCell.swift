0c5c2b267aa0c75d83d95e148a9b57814092de97
Haena <ghkwlsspdl@naver.com>
2023-06-21 14:38:11 +0900

    [#130] FEAT: 상품리스트 셀 타이틀 세팅
    - 타이틀 폰트 등등 세팅
    - 레이아웃세팅

--- iOS/second-hand/second-hand/Home/Controller/HomeProductCollectionViewCell.swift
+++ iOS/second-hand/second-hand/Home/Controller/HomeProductCollectionViewCell.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class HomeProductCollectionViewCell: UICollectionViewCell {
     static let identifier = "productCell"
-      let imageView = UIImageView()
-      var title = UILabel()
-      let location = UILabel()
-      let dot = UILabel()
-      let registerTime = UILabel()
-      let statusLabel = UILabel()
-      let price = UILabel()
-      let chat = UILabel()
-      let wish = UILabel()
-      let line = UILabel()
+    let imageView = UIImageView()
+    var title = UILabel()
+    let location = UILabel()
+    let dot = UILabel()
+    let registerTime = UILabel()
+    let statusLabel = UILabel()
+    let price = UILabel()
+    let chat = UILabel()
+    let wish = UILabel()
+    let line = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUI()
-        //layout()
+        layout()
     }
     
     required init?(coder: NSCoder) {
@@ -32,15 +32,21 @@ class HomeProductCollectionViewCell: UICollectionViewCell {
     
     private func setUI() {
         setImageView()
+        setTitle()
     }
     
     private func setImageView() {
-            imageView.layer.borderWidth = 1
-            imageView.layer.borderColor = .init(red: 179/255, green: 179/255, blue: 179/255, alpha: 0.39)
-            
-            imageView.layer.masksToBounds = true
-            imageView.layer.cornerRadius = 8
-        }
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = .init(red: 179/255, green: 179/255, blue: 179/255, alpha: 0.39)
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 8
+    }
+    
+    private func setTitle() {
+        title.font = .subHead
+        title.textColor = .neutralText
+    }
     
     func layout() {
         [imageView, title, location, dot, registerTime, statusLabel, price, chat, wish, line].forEach{
@@ -59,7 +65,11 @@ class HomeProductCollectionViewCell: UICollectionViewCell {
             imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
             imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
             imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16),
-            imageView.widthAnchor.constraint(equalToConstant: height-32)
+            imageView.widthAnchor.constraint(equalToConstant: height-32),
+            
+            title.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15*widthRatio),
+            title.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 4*heightRatio),
+            
         ])
     }
 }
