762a7b8b4545cecc0deb2a0162bfd5b6d2ecc77f
Haena <ghkwlsspdl@naver.com>
2023-06-22 18:14:52 +0900

    [#146] MOVE: 폴더링
    - 각각에 알맞는 위치로 폴더링

---
+++ iOS/second-hand/second-hand/Home/subScene/RegisterNewProduct/View/RegisterProductScrollVeiw.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterProductScrollVeiw.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/06/22.
+//
+
+import UIKit
+
+class RegisterProductScrollVeiw: UIScrollView {
+    var stackView = UIStackView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
