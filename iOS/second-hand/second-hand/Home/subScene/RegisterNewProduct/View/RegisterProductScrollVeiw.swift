3193cfe2878b7dd99025d3ff2923e56a8ae85884
Haena <ghkwlsspdl@naver.com>
2023-06-23 00:19:04 +0900

    [#146] FEAT: ProductImage 세팅
    - 스크롤뷰를 이용해 세팅중

--- iOS/second-hand/second-hand/Home/subScene/RegisterNewProduct/View/RegisterProductScrollVeiw.swift
+++ iOS/second-hand/second-hand/Home/subScene/RegisterNewProduct/View/RegisterProductScrollVeiw.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class RegisterProductScrollVeiw: UIScrollView {
+    var productView = UIImageView.makeSquare(width: 80, height: 80, radius: 12, image: UIImage(systemName: "photo") ?? UIImage())
     var stackView = UIStackView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setStackView()
+        stackView.spacing = 16
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setStackView() {
+        stackView.addArrangedSubview(productView)
+    }
+    
+    private func configure() {
+        self.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.heightAnchor.constraint(equalTo: self.heightAnchor),
+            stackView.topAnchor.constraint(equalTo: self.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        //스크롤바 가리기
+        self.showsHorizontalScrollIndicator = false
+    }
 }
+
