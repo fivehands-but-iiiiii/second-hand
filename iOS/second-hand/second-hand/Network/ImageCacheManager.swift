1c03a93f02cd799cee3a05cb1422d7f3d7390d2e
SONG <geun2121@gmail.com>
2023-06-27 20:32:18 +0900

    [#201] FEAT: ImageCacheManager 추가
    - singleTon 
    - key와 Image 맵핑

---
+++ iOS/second-hand/second-hand/Network/ImageCacheManager.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCacheManager.swift
+//  second-hand
+//
+//  Created by SONG on 2023/06/27.
+//
+
+import UIKit
+
+class ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+    
+    private init() {
+        
+    }
+}
