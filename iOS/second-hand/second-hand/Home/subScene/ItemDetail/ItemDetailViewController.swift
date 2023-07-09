5e35671ffd579e48684bd3560b910d75dbc2dbf1
SONG <geun2121@gmail.com>
2023-07-09 20:21:53 +0900

    [#244] ADD: 상세화면 ViewController 추가
    - Home/subScene/ItemDetail 경로에 추가

---
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/ItemDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemDetailViewController.swift
+//  second-hand
+//
+//  Created by SONG on 2023/07/09.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
