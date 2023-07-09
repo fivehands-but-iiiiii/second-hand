0a99500319fd20d09db7224b97f83ef3ae01c002
SONG <geun2121@gmail.com>
2023-07-10 03:15:02 +0900

    [#244] FEAT: menuButton 구현
    - 우측상단 "..." 버튼 구현

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/Controller/ItemDetailViewController.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/Controller/ItemDetailViewController.swift
@@ -9,25 +9,37 @@ import UIKit
 
 class ItemDetailViewController: UIViewController {
     private var backButton: UIButton? = nil
+    private var menuButton: UIButton? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setInitialScene()
+        initializeScene()
     }
     
-    private func setInitialScene() {
+    private func initializeScene() {
         self.view.backgroundColor = .white
         setNavigationBatHidden()
         generateBackButton()
+        generateMenuButton()
         setConstraintsBackButton()
+        setConstraintsMenuButton()
     }
     
     private func generateBackButton() {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
-        button.tintColor = .gray
+        button.tintColor = .black
         button.addTarget(self, action: #selector(backButtonTouched), for: .touchUpInside)
         self.backButton = button
+    }
+    
+    private func generateMenuButton() {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "ellipsis"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action:#selector(menuButtonTouched), for: .touchUpInside)
+        self.menuButton = button
+        
     }
     
     private func setConstraintsBackButton() {
@@ -38,8 +50,21 @@ class ItemDetailViewController: UIViewController {
         self.view.addSubview(backButton)
         
         backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+        backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
         backButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+    }
+    
+    private func setConstraintsMenuButton() {
+        guard let menubutton = self.menuButton else {
+            return
+        }
+        
+        self.view.addSubview(menubutton)
+        
+        menubutton.translatesAutoresizingMaskIntoConstraints = false
+        
+        menubutton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        menubutton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
     }
     
     private func setNavigationBatHidden() {
@@ -48,5 +73,9 @@ class ItemDetailViewController: UIViewController {
     
     @objc private func backButtonTouched() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func menuButtonTouched() {
+        
     }
 }
