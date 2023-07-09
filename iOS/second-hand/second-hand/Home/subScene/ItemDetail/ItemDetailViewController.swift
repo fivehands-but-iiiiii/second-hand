89c50b9fb9735769937231215b2e5a0147c8e9eb
SONG <geun2121@gmail.com>
2023-07-09 21:07:40 +0900

    [#244] FEAT: 상세화면 초기세팅
    - self.view backgroundColor 변경 
    - navigationBarButton 교체 
    - "<" 버튼 터치시 이전 뷰컨트롤러로 복귀 구현

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/ItemDetailViewController.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/ItemDetailViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ItemDetailViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setInitialScene()
     }
-
+    
+    private func setInitialScene() {
+        self.view.backgroundColor = .white
+        setBarButton()
+    }
+    
+    private func setBarButton() {
+        let backButton = UIBarButtonItem(image:UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonTouched))
+        backButton.tintColor = .gray
+        navigationItem.setLeftBarButton(backButton, animated: true)
+    }
+    
+    @objc private func backButtonTouched() {
+        navigationController?.popViewController(animated: true)
+    }
 }
