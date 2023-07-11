7fc332ed0af8b17447fe66211085f21ce9aaf8dc
SONG <geun2121@gmail.com>
2023-07-11 18:29:08 +0900

    [#244] FEAT: commonInit() 메소드 구현
    - 각 subView에 대한 프로퍼티 세팅

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ItemDetailImagePages.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ItemDetailImagePages.swift
@@ -8,27 +8,41 @@
 import UIKit
 
 class ItemDetailImagePages: UIView {
+    private var scrollView = UIScrollView()
+    private var images: [String] = []
+    private var imageView : UIImageView!
     private var pageControl = UIPageControl(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        scrollView.delegate = self
+        scrollView.isScrollEnabled = true
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        addSubview(scrollView)
+
+//        imageView.contentMode = .scaleAspectFit
+//        imageView.image = UIImage(systemName: "paperplane")
+//        scrollView.addSubview(imageView)
         
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            commonInit()
-        }
-        
-        required init?(coder: NSCoder) {
-            super.init(coder: coder)
-            commonInit()
-        }
-        
-        private func commonInit() {
-            pageControl.numberOfPages = 2
-            pageControl.backgroundColor = .blue
-            pageControl.backgroundStyle = .automatic
-            addSubview(pageControl)
-        }
-        
-        override func layoutSubviews() {
-            super.layoutSubviews()
+        pageControl.numberOfPages = 2
+        pageControl.backgroundColor = .clear
+        pageControl.backgroundStyle = .automatic
+        pageControl.currentPageIndicatorTintColor = .black
+        pageControl.pageIndicatorTintColor = .gray
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
             setConstraintsPageControl()
         }
         
