cc47d55d94b0583f7b96e5a648dcecf9d8e39403
Haena <ghkwlsspdl@naver.com>
2023-07-11 17:17:32 +0900

    [#248] FEAT: 채팅UI 테이블뷰 세팅
    - 델리게이트 설정
    - 레이아웃 설정

--- iOS/second-hand/second-hand/Chatting/Controller/ChattingViewController.swift
+++ iOS/second-hand/second-hand/Chatting/Controller/ChattingViewController.swift
@@ -7,15 +7,25 @@
 
 import UIKit
 
-final class ChattingViewController: NavigationUnderLineViewController {
+enum ChattingSection: CaseIterable {
+    case main
+}
 
+final class ChattingViewController: NavigationUnderLineViewController {
+    
+    private let tableView = UITableView()
+    private let chattingRoomTableViewCell = ChattingRoomTableViewCell(style: .default, reuseIdentifier: ChattingRoomTableViewCell.identifier)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(ChattingRoomTableViewCell.self, forCellReuseIdentifier: ChattingRoomTableViewCell.identifier)
+        setTableView()
+        layout()
         self.navigationItem.title = "채팅"
         
         let stompManager = StompManager()
         stompManager.registerSocket() // 소켓 등록
-
+        
         // 메시지 보내기
         let messagePayload: [String: Any] = [
             "content": "333",
@@ -23,7 +33,23 @@ final class ChattingViewController: NavigationUnderLineViewController {
         ]
         let destination = "/pub/chat/message"
         stompManager.sendMessage(payloadObject: messagePayload, destination: destination)
-
+        
     }
-
+    
+    private func setTableView() {
+           tableView.delegate = self
+           tableView.dataSource = self
+       }
+       
+       private func layout() {
+           self.view.addSubview(tableView)
+           tableView.translatesAutoresizingMaskIntoConstraints = false
+           
+           NSLayoutConstraint.activate([
+               tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+               tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+               tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+               tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+           ])
+       }
 }
