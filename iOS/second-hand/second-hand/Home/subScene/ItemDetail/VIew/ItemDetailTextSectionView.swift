0e54dd30854319b430abca44de7e308e0d5b8e25
SONG <geun2121@gmail.com>
2023-07-15 22:53:02 +0900

    [#244] FEAT: SellerInfoView 생성 및 layout설정
    - SellerInfoView 생성 , 파라미터로 sellerName 사용
    - SellerInfoView layout 세팅

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ItemDetailTextSectionView.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ItemDetailTextSectionView.swift
@@ -18,5 +18,39 @@ class ItemDetailTextSectionView: UIScrollView {
         super.init(coder: coder)
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+//        self.contentSize = CGSize(width: self.frame.width, height: self.frame.height)
+        setSellerInfoConstraints()
 
+    }
+    
+    func setSellerInfoView(sellerName: String) {
+        self.sellerInfoView = SellerInfoView(sellerName: sellerName)
+        setSellerInfoConstraints()
+    }
+    
+    
+    private func setSellerInfoConstraints() {
+        guard let sellerInfoView = sellerInfoView else {
+            return
+        }
+        
+        if !self.subviews.contains(sellerInfoView) {
+            self.addSubview(sellerInfoView)
+        }
+ 
+        sellerInfoView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                sellerInfoView.topAnchor.constraint(equalTo: self.topAnchor),
+                sellerInfoView.widthAnchor.constraint(equalToConstant: self.frame.width - 30),
+                sellerInfoView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 54/491),
+                sellerInfoView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            ]
+        )
+        
+    }
+    
 }
