83703d52dd4df080f366ed90590282097104b90d
SONG <geun2121@gmail.com>
2023-07-16 02:04:24 +0900

    [#244] FEAT: StatusButton 파일생성
    - 내부 subviews 변수 선언 및 생성

---
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/StatusButton.swift
@@ -0,0 +1,15 @@
+//
+//  StatusButton.swift
+//  second-hand
+//
+//  Created by SONG on 2023/07/16.
+//
+
+import UIKit
+
+class StatusButton: UIButton {
+    private var status : String? = nil
+    private var label = UILabel(frame: .zero)
+    private var sideImage = UIImageView(frame: .zero)
+    
+}
