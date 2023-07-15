a36d82e82d13977d531a6b9b070fc98663bf6b1f
SONG <geun2121@gmail.com>
2023-07-16 02:06:15 +0900

    [#244] FEAT: setLabel() 메소드 작성
    - status 값을 기반으로 텍스트 지정 
    - size , color 등 지정

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/StatusButton.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/StatusButton.swift
@@ -12,4 +12,14 @@ class StatusButton: UIButton {
     private var label = UILabel(frame: .zero)
     private var sideImage = UIImageView(frame: .zero)
     
+    
+    private func setLabel() {
+        label.text = self.status
+        label.font = UIFont.systemFont(ofSize: 13.0)
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        label.textColor = .black
+        self.addSubview(label)
+    }
+    
 }
