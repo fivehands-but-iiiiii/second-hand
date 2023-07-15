513a99a7656e8403a385b17cc13872e2f40ff424
SONG <geun2121@gmail.com>
2023-07-16 02:07:02 +0900

    [#244] FEAT: status값에 따라 출력텍스트 분기

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/StatusButton.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/StatusButton.swift
@@ -12,6 +12,18 @@ class StatusButton: UIButton {
     private var label = UILabel(frame: .zero)
     private var sideImage = UIImageView(frame: .zero)
     
+    private func setInitialStatus(status : Int){
+        switch status{
+        case 0:
+            self.status = "판매중"
+        case 1:
+            self.status = "예약중"
+        case 2:
+            self.status = "판매완료"
+        default:
+            self.status = "unknown"
+        }
+    }
     
     private func setLabel() {
         label.text = self.status
