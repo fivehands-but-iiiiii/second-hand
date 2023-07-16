4a04550dfc3ad0b0519cc9bf4583ce81e3a5234b
SONG <geun2121@gmail.com>
2023-07-16 19:51:21 +0900

    [#244] FEAT: TitleLabel
    - 모델에서 값을 가져온 뒤 add subview
    - TitleLabel layout 세팅

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ContentOfPost.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ContentOfPost.swift
@@ -8,13 +8,58 @@
 import UIKit
 
 class ContentOfPost: UIView {
+    private var titleLabel : UILabel? = nil
+    private var categotyAndCreateAtLabel : UILabel? = nil
+    private var descriptionView : UITextView? = nil
+    private var countsLabel : UILabel? = nil
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init(title: String, category: Int, createAt:String, content: String, chatCount: Int, likeCount: Int, hits: Int) {
+        super.init(frame: .zero)
+        setTitleLabel(title: title)
+        
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setTitleLabelConstraints()
+    }
+    // MARK: TITLE
+    
+    private func setTitleLabel(title: String) {
+        self.titleLabel = UILabel(frame: .zero)
+        self.titleLabel?.text = title
+        self.titleLabel?.font = .headLine
+    }
+    
+    private func setTitleLabelConstraints() {
+        guard let titleLabel = titleLabel else {
+            return
+        }
+        
+        guard let text = titleLabel.text else {
+            return
+        }
+        
+        if !self.subviews.contains(titleLabel) {
+            self.addSubview(titleLabel)
+        }
+        
+        let width = CGFloat(text.count * 15)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
+                titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                titleLabel.widthAnchor.constraint(equalToConstant: width),
+                titleLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.05)
+            ]
+        )
+    }
+    
 }
