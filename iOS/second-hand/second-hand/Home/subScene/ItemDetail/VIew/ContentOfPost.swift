ab716ebf29ed1ec232926bb556cb4208f0e345fb
SONG <geun2121@gmail.com>
2023-07-16 20:19:32 +0900

    [#244] FEAT: CategotyAndCreateAtLabel
    - 생성시 모델 동기화 
    - text 변환 메소드 작성 
    - layoutSubview에서 layout 잡기

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ContentOfPost.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ContentOfPost.swift
@@ -16,6 +16,7 @@ class ContentOfPost: UIView {
     init(title: String, category: Int, createAt:String, content: String, chatCount: Int, likeCount: Int, hits: Int) {
         super.init(frame: .zero)
         setTitleLabel(title: title)
+        setCategotyAndCreateAtLabel(category: category, createAt: createAt)
         
     }
     
@@ -26,6 +27,7 @@ class ContentOfPost: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         setTitleLabelConstraints()
+        setCategotyAndCreateAtLabelConstraints()
     }
     // MARK: TITLE
     
@@ -62,4 +64,94 @@ class ContentOfPost: UIView {
         )
     }
     
+    // MARK: categotyAndCreateAtLabel
+    
+    private func setCategotyAndCreateAtLabel(category: Int, createAt:String) {
+        
+        let convertedCategory = convertCategoryIntToString(category)
+        let convertedCreateAt = createAt.convertToRelativeTime()
+        
+        let text = convertedCategory + " • " + convertedCreateAt
+        self.categotyAndCreateAtLabel = UILabel(frame: .zero)
+        self.categotyAndCreateAtLabel?.text = text
+        self.categotyAndCreateAtLabel?.font = .footNote
+        self.categotyAndCreateAtLabel?.font = .systemFont(ofSize: 13.0)
+        self.categotyAndCreateAtLabel?.textColor = .lightGray
+    }
+    
+    private func setCategotyAndCreateAtLabelConstraints() {
+        guard let categotyAndCreateAtLabel = categotyAndCreateAtLabel else {
+            return
+        }
+        
+        guard let text = categotyAndCreateAtLabel.text else {
+            return
+        }
+        
+        guard let standard = self.titleLabel?.bottomAnchor else {
+            return
+        }
+        
+        if !self.subviews.contains(categotyAndCreateAtLabel) {
+            self.addSubview(categotyAndCreateAtLabel)
+        }
+        
+        let width = CGFloat(text.count * 13)
+        
+        categotyAndCreateAtLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                categotyAndCreateAtLabel.topAnchor.constraint(equalTo: standard ,constant: 5.0),
+                categotyAndCreateAtLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                categotyAndCreateAtLabel.widthAnchor.constraint(equalToConstant: width),
+                categotyAndCreateAtLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.05)
+            ]
+        )
+    }
+}
+
+extension ContentOfPost {
+    private func convertCategoryIntToString(_ num: Int) -> String{
+        switch num {
+        case 1 :
+            return "디지털기기"
+        case 2 :
+            return "생활가전"
+        case 3 :
+            return "가구/인테리어"
+        case 4 :
+            return "생활/주방"
+        case 5 :
+            return "유아동"
+        case 6 :
+            return "유아도서"
+        case 7 :
+            return "여상의류"
+        case 8 :
+            return "여성잡화"
+        case 9 :
+            return "남성패션/잡화"
+        case 10 :
+            return "뷰티/미용"
+        case 11 :
+            return "스포츠/레저"
+        case 12 :
+            return "취미/게임/음반"
+        case 13 :
+            return "중고차"
+        case 14 :
+            return "티켓/교환권"
+        case 15 :
+            return "가공식품"
+        case 16 :
+            return "반려동물용품"
+        case 17 :
+            return "식물"
+        case 18 :
+            return "기타 중고물품"
+        default:
+            return "unknown"
+        }
+    }
 }
