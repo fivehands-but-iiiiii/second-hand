87b2b17cf19949c23bd4c034aa31fc9ee8f16963
SONG <geun2121@gmail.com>
2023-07-16 19:07:19 +0900

    [#244] ADD: 게시글내용 파일 추가
    - ContentOfPost class 파일을 추가하였다.

---
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ContentOfPost.swift
@@ -0,0 +1,20 @@
+//
+//  ContentOfPost.swift
+//  second-hand
+//
+//  Created by SONG on 2023/07/16.
+//
+
+import UIKit
+
+class ContentOfPost: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
