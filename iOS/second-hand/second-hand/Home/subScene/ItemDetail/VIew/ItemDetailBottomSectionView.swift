c0ee25e1b86ab28adecb4b869f793054d1e465b8
SONG <geun2121@gmail.com>
2023-07-17 03:09:53 +0900

    [#244] FEAT: LikeButton 추가
    - isLike 에 따라 다른 image 세팅 
    - property , layout 세팅

--- iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ItemDetailBottomSectionView.swift
+++ iOS/second-hand/second-hand/Home/subScene/ItemDetail/VIew/ItemDetailBottomSectionView.swift
@@ -14,10 +14,16 @@ class ItemDetailBottomSectionView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = .systemBackgroundWeak
+        self.layer.borderWidth = 0.6
+        self.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        self.backgroundColor = .systemBackgroundWeak
+        self.layer.borderWidth = 0.6
+        self.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     override func layoutSubviews() {
@@ -27,11 +33,36 @@ class ItemDetailBottomSectionView: UIView {
         setChattingRoomButtonConstraints()
     }
     func setLikeButton(isLike: Bool) {
+        self.likeButton = UIButton(type: .system)
         
+        switch isLike {
+        case true:
+            self.likeButton?.tintColor = .red
+            self.likeButton?.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        default:
+            self.likeButton?.tintColor = .black
+            self.likeButton?.setImage(UIImage(systemName: "heart"), for: .normal)
+        }
     }
     
     private func setsetLikeButtonConstraints() {
+        guard let likeButton = likeButton else {
+            return
+        }
         
+        if !self.subviews.contains(likeButton) {
+            self.addSubview(likeButton)
+        }
+        
+        likeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                likeButton.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 28/83),
+                likeButton.widthAnchor.constraint(equalTo: likeButton.heightAnchor),
+                likeButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15.0),
+                likeButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 10)
+            ]
+        )
     }
     
     func setPriceLabel(price: Int) {
