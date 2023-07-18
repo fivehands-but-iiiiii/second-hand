c5ae3b3d49065c7c4a1f8e288595019cc6296326
saehim <saeilee.vxv@gmail.com>
2023-07-18 17:00:56 +0900

    REFACTOR: 필요없는 파일 삭제

--- front-end/src/components/home/category/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './category';
-export type { CategoryInfo } from './category';
--- front-end/src/styles/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-import { ColorsType, FontsType } from './theme';
-
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: ColorsType;
-    fonts: FontsType;
-  }
-}
