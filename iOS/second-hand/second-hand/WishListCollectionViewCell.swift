3b5afce0eda9084dfa2b13d364d9dec58f127de6
Haena <ghkwlsspdl@naver.com>
2023-07-21 00:10:16 +0900

    [#284] ADD: 관심목록 컬렉션뷰 세팅
    - 레이아웃 세팅
    - 델리게이트 채택
    - 카테고리스크롤뷰 컨스트레인트 재설정

---
+++ iOS/second-hand/second-hand/WishListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WishListCollectionViewCell.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/07/20.
+//
+
+import UIKit
+
+class WishListCollectionViewCell: UICollectionViewCell {
+    static let identifier = "wishListIdentifier"
+}
--- iOS/second-hand/second-hand/WishListViewController.swift
+++ iOS/second-hand/second-hand/WishListViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 final class WishListViewController: NavigationUnderLineViewController {
     private var categoryScrollView = CategoryScrollView()
+    private var wishlistCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        setCategory()
+        setCollectionView()
         makeButton(category: "가구/인테리어")
         makeButton(category: "안녕안녕")
         makeButton(category: "하하하")
@@ -19,7 +21,7 @@ final class WishListViewController: NavigationUnderLineViewController {
         makeButton(category: "길게길게길게길게길게길게")
     }
     
-    private func setupUI() {
+    private func setCategory() {
         //네비게이션
         self.navigationItem.title = "관심 목록"
         let allCategoriButton = CategoryButton(title: "전체")
@@ -28,6 +30,16 @@ final class WishListViewController: NavigationUnderLineViewController {
         allCategoriButton.setTitleColor(UIColor.white, for: .normal)
         allCategoriButton.layer.borderWidth = 0
         categoryLayout()
+    }
+    
+    private func setCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: self.view.frame.width, height: 150)
+        wishlistCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        wishlistCollectionView.delegate = self
+        wishlistCollectionView.dataSource = self
+        wishlistCollectionView.register(WishListCollectionViewCell.self, forCellWithReuseIdentifier: WishListCollectionViewCell.identifier)
+        collectionViewConstraint()
     }
     
     private func categoryLayout() {
@@ -41,7 +53,7 @@ final class WishListViewController: NavigationUnderLineViewController {
         let heightRatio: CGFloat = height/figmaHeight
         let widthRatio: CGFloat = width/figmaWidth
         NSLayoutConstraint.activate([
-            categoryScrollView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 122*heightRatio),
+            categoryScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10*heightRatio),
             categoryScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16*widthRatio),
             categoryScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16*widthRatio),
             categoryScrollView.heightAnchor.constraint(equalToConstant: 32*heightRatio)
@@ -51,5 +63,32 @@ final class WishListViewController: NavigationUnderLineViewController {
     private func makeButton(category: String) {
         let categoryLabel = CategoryButton(title: category)
         categoryScrollView.categoriStackView.addArrangedSubview(categoryLabel)
+    }
+    
+    private func collectionViewConstraint() {
+        self.view.addSubview(wishlistCollectionView)
+        wishlistCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            wishlistCollectionView.topAnchor.constraint(equalTo: categoryScrollView.bottomAnchor, constant: 10),
+            wishlistCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+            wishlistCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
+            wishlistCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
+extension WishListViewController: UICollectionViewDelegate {
+    
+}
+
+extension WishListViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WishListCollectionViewCell.identifier, for: indexPath) as? WishListCollectionViewCell
+        return cell ?? UICollectionViewCell()
     }
 }
