75d9a50deef5da740ec8e93f238968d3c9dc2292
Haena <ghkwlsspdl@naver.com>
2023-07-27 16:46:22 +0900

    [#318] FEAT: 카테고리 화면을 벗어날때 show탭바 실행
    - 카테고리화면에서 없앴던 탭바를 다시 보이도록 만듬

--- iOS/second-hand/second-hand/CategoryViewController.swift
+++ iOS/second-hand/second-hand/CategoryViewController.swift
@@ -8,28 +8,38 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        showTabBar()
+    }
+    
     private func setUI() {
-          self.view.backgroundColor = .white
-          setNavigationBar()
-          hideTabBar()
-      }
-      
-      private func setNavigationBar() {
-          self.navigationItem.title = "카테고리"
-          
-          navigationController?.navigationBar.tintColor = .black
-          navigationController?.navigationBar.topItem?.backButtonTitle = "뒤로"
-      }
-      
-      private func hideTabBar() {
-          tabBarController?.tabBar.isHidden = true
-      }
-      
+        self.view.backgroundColor = .white
+        setNavigationBar()
+        hideTabBar()
+    }
+    
+    private func setNavigationBar() {
+        self.navigationItem.title = "카테고리"
+        
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.topItem?.backButtonTitle = "뒤로"
+    }
+    
+    private func hideTabBar() {
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    private func showTabBar() {
+        tabBarController?.tabBar.isHidden = false
+        
+    }
+    
 }
