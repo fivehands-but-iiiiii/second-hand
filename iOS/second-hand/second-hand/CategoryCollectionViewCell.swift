ca68e071a934abab9a37fbd95b9267a01b252214
Haena <ghkwlsspdl@naver.com>
2023-07-27 16:53:07 +0900

    [#318] ADD: 카테고리컬렉션뷰셀 레이아웃
    - 컨스트레인트를 기준으로 레이아웃 진행

--- iOS/second-hand/second-hand/CategoryCollectionViewCell.swift
+++ iOS/second-hand/second-hand/CategoryCollectionViewCell.swift
@@ -22,7 +22,22 @@ class CategoryCollectionViewCell: UICollectionViewCell {
             layout()
         }
     
-    func layout() {
-        
-    }
+    private func layout() {
+            [image, title].forEach{
+                self.contentView.addSubview($0)
+                $0.translatesAutoresizingMaskIntoConstraints = false
+            }
+            
+            NSLayoutConstraint.activate([
+                image.topAnchor.constraint(equalTo: self.topAnchor),
+                image.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                image.heightAnchor.constraint(equalToConstant: 44),
+                image.widthAnchor.constraint(equalToConstant: 44),
+                
+                title.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 4),
+                title.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+                title.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                title.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+                ])
+        }
 }
