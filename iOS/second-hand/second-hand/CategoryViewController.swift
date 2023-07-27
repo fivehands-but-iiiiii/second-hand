b926ffbdadc2874130acbbca53c57fcdb6620e7f
Haena <ghkwlsspdl@naver.com>
2023-07-27 17:08:02 +0900

    [#318] FEAT: 컬렉션뷰 델리게이트 채택
    - dataSource 델리게이트 채택 및 세팅
    - flowLayout 델리게이트 채택 및 세팅

--- iOS/second-hand/second-hand/CategoryViewController.swift
+++ iOS/second-hand/second-hand/CategoryViewController.swift
@@ -8,18 +8,19 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
+    private let categoryList = Category.list
     private var categoryListCollectionView = UICollectionView(frame: .zero,collectionViewLayout: UICollectionViewFlowLayout())
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setUI()
+        setCategoryCollectionView()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        showTabBar()
-    }
+            super.viewWillDisappear(animated)
+            showTabBar()
+        }
     
     private func setUI() {
         self.view.backgroundColor = .white
@@ -40,19 +41,48 @@ class CategoryViewController: UIViewController {
     
     private func showTabBar() {
         tabBarController?.tabBar.isHidden = false
+        
+    }
+
+    private func setCategoryCollectionView() {
+        categoryListCollectionView.dataSource = self
+        categoryListCollectionView.delegate = self
+        categoryListCollectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+
+        let margin: CGFloat = 40
+        self.view.addSubview(categoryListCollectionView)
+        categoryListCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            categoryListCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: margin),
+            categoryListCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            categoryListCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
+            categoryListCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
+                ])
+    }
+}
+
+extension CategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        categoryList.count
     }
     
-    private func setCategoryCollectionView() {
-            categoryListCollectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as? CategoryCollectionViewCell
 
-            let margin: CGFloat = 40
-            self.view.addSubview(categoryListCollectionView)
-            categoryListCollectionView.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                categoryListCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: margin),
-                categoryListCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-                categoryListCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: margin),
-                categoryListCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -margin),
-                    ])
+        return cell ?? UICollectionViewCell()
+    }
+    
+}
+
+extension CategoryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let cellWidth: CGFloat = 80
+            let cellHeight: CGFloat = 68
+
+            return CGSize(width: cellWidth, height: cellHeight)
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 40
         }
 }
