6cdb50b21e20b607639a8a1863d0592d22e6a9cc
Haena <ghkwlsspdl@naver.com>
2023-07-27 16:44:02 +0900

    [#318] FEAT: 카테고리 화면 네비게이션바/탭바 세팅
    - 네비게이션바 백 버튼 타이틀 변경
    - 탭바 숨기기
    - 네비게이션 타이틀 변경

--- iOS/second-hand/second-hand/CategoryViewController.swift
+++ iOS/second-hand/second-hand/CategoryViewController.swift
@@ -11,6 +11,25 @@ class CategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
     }
 
+    private func setUI() {
+          self.view.backgroundColor = .white
+          setNavigationBar()
+          hideTabBar()
+      }
+      
+      private func setNavigationBar() {
+          self.navigationItem.title = "카테고리"
+          
+          navigationController?.navigationBar.tintColor = .black
+          navigationController?.navigationBar.topItem?.backButtonTitle = "뒤로"
+      }
+      
+      private func hideTabBar() {
+          tabBarController?.tabBar.isHidden = true
+      }
+      
 }
