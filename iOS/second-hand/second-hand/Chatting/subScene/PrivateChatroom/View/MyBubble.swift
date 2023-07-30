718df619a9805901c820d3588ecd94df1f6aa25b
SONG <geun2121@gmail.com>
2023-07-30 18:33:05 +0900

    [#335] FEAT: MyBubble 초기화 메소드 작성
    - set image 
    - set textBox 
    - layout constraints

--- iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/View/MyBubble.swift
+++ iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/View/MyBubble.swift
@@ -8,5 +8,40 @@
 import UIKit
 
 class MyBubble: UIImageView {
+    private let bubbleImage = UIImage(named: "bubbleOragne.png")?.resizableImage(withCapInsets: UIEdgeInsets(top: 5.0, left: 5.0, bottom: 5.0, right: 5.0), resizingMode: .stretch)
     
+    private var textBox = UITextView(frame: .zero)
+    
+    init(text:String){
+        super.init(frame: .zero)
+        self.image = bubbleImage
+        setTextBox(text: text)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.image = bubbleImage
+    }
+    
+    override func layoutSubviews() {
+        setConsraintsTextBox()
+    }
+    
+    private func setTextBox(text:String) {
+        self.textBox.isScrollEnabled = false
+        self.textBox.isEditable = false
+        self.textBox.text = text
+        self.textBox.font = .systemFont(ofSize: 17.0)
+        self.textBox.textColor = .systemBackground
+        self.textBox.textAlignment = .right
+    }
+    
+    private func setConsraintsTextBox() {
+        self.textBox.sizeToFit()
+        
+        if !self.subviews.contains(textBox) {
+            self.addSubview(textBox)
+        }
+        
+    }
 }
