d27f6eccd3fe21b72d9fa2955fd34298e71b43bb
Haena <ghkwlsspdl@naver.com>
2023-07-30 11:43:01 +0900

    [#318] ADD: 컬렉션뷰 생성 및 레이아웃
    - 컬렉션뷰 생성
    - flow레이아웃 세팅
    - 컬렉션뷰 컨스트레인트 세팅

--- iOS/second-hand/second-hand/Home/subScene/Category/subScene/Controller/CategoryItemListViewController.swift
+++ iOS/second-hand/second-hand/Home/subScene/Category/subScene/Controller/CategoryItemListViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class CategoryItemListViewController: UIViewController {
     private var category: Int?
+    private var productListCollectionView = UICollectionView(frame: .zero,collectionViewLayout: UICollectionViewFlowLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setCollectionView()
     }
     
     private func setUI() {
@@ -25,6 +27,30 @@ class CategoryItemListViewController: UIViewController {
         self.navigationItem.title = stringCategory
         
         navigationController?.navigationBar.tintColor = .black
+    }
+    
+    private func setCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        let figmaCellHight = 152
+        let figmaHeight = 852
+        
+        layout.minimumLineSpacing = 1.1
+        layout.itemSize = .init(width: self.view.frame.width, height: CGFloat(figmaCellHight*figmaHeight)/self.view.frame.height)
+        productListCollectionView.setCollectionViewLayout(layout, animated: true)
+        self.view.addSubview(productListCollectionView)
+        setCollectionViewConstraints()
+    }
+    
+    private func setCollectionViewConstraints() {
+        productListCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                productListCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+                productListCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                productListCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+                productListCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+            ]
+        )
     }
     
     private func hideTabBar() {
