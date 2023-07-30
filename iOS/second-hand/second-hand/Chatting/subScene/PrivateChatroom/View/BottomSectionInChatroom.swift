50088da3577634cbbb51a3b9cb8a68af85a5ba33
SONG <geun2121@gmail.com>
2023-07-30 16:12:50 +0900

    [#335] ADD: 채팅방UI
    - 채팅방 bottomSectionView 생성

---
+++ iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/View/BottomSectionInChatroom.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSectionInChatroom.swift
+//  second-hand
+//
+//  Created by SONG on 2023/07/30.
+//
+
+import UIKit
+
+class BottomSectionInChatroom: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
