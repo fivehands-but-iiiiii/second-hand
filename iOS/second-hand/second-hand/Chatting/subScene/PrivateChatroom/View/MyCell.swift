136c6de69b093517c35d8ba82f570c5b720568c8
SONG <geun2121@gmail.com>
2023-07-30 18:34:00 +0900

    [#335] FEAT: MyCell 초기화 메소드 작성
    - set bubble
    - set identifier 
    - layout constraints

--- iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/View/MyCell.swift
+++ iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/View/MyCell.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 class MyCell: UITableViewCell {
-
+    static let identifier = "MYCELL"
+    
+    private var bubble : MyBubble? = nil
+    
+    init(text:String) {
+        super.init(style: .default, reuseIdentifier: MyCell.identifier)
+        self.bubble = MyBubble(text: text)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        setConstraintsBubble()
+    }
+    
+    private func setConstraintsBubble() {
+        guard let bubble = bubble else {
+            return
+        }
+        
+        if !self.contentView.subviews.contains(bubble) {
+            self.contentView.addSubview(bubble)
+        }
+        
+        bubble.sizeToFit()
+        bubble.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                bubble.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor,constant: -5.0),
+                bubble.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor)
+            ]
+        )
+    }
 }
