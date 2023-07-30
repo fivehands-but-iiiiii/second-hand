5f0b5e5e428250cdaa5a75d221f587902183da35
Haena <ghkwlsspdl@naver.com>
2023-07-31 00:25:06 +0900

    [#336] FIX: 위시리스트 찜 해제시 아이템리스트 재배열 안되는 버그 해결
    - apear마다 page=0으로 초기화를 시키며 해결

--- iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/Controller/PrivateChatroomViewController.swift
+++ iOS/second-hand/second-hand/Chatting/subScene/PrivateChatroom/Controller/PrivateChatroomViewController.swift
@@ -1,201 +1,201 @@
+////
+////  ChattingRoomViewController.swift
+////  second-hand
+////
+////  Created by SONG on 2023/07/19.
+////
 //
-//  ChattingRoomViewController.swift
-//  second-hand
+//import UIKit
 //
-//  Created by SONG on 2023/07/19.
+//class PrivateChatroomViewController: UIViewController {
+//    private var backButton: UIButton? = nil
+//    private var menuButton: UIButton? = nil
+//    private var chatroomTitle: UILabel? = nil
+//    var privateChatroomModel = PrivateChatroomModel()
+//    private var itemSummary: ItemSummaryInChatroom? = nil
+//    private var socketManager : SocketManager? = nil
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        commonInit()
+//        readyToChat()
+//    }
 //
-
-import UIKit
-
-class PrivateChatroomViewController: UIViewController {
-    private var backButton: UIButton? = nil
-    private var menuButton: UIButton? = nil
-    private var chatroomTitle: UILabel? = nil
-    var privateChatroomModel = PrivateChatroomModel()
-    private var itemSummary: ItemSummaryInChatroom? = nil
-    private var socketManager : SocketManager? = nil
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        commonInit()
-        readyToChat()
-    }
-
-    private func commonInit() {
-        self.view.backgroundColor = .systemBackground
-        generateBackButton()
-        generateMenuButton()
-        setConstraintsBackButton()
-        setConstraintsMenuButton()
-        didUpdateModel()
-        
-    }
-    
-    private func readyToChat() {
-        
-        guard let roomId = privateChatroomModel.info?.chatroomId else {
-            return
-        }
-        
-        guard let sender = UserInfoManager.shared.userInfo?.memberId else {
-            return
-        }
- 
-        self.socketManager = SocketManager(roomId: roomId, sender: sender)
-        //socketManager.sendTest(message: "안녕")
-    }
-    
-    
-    
-    private func didUpdateModel() {
-        
-        setChatroomTitle()
-        setItemSummary()
-    }
-    
-    private func setItemSummary() {
-        guard let itemImageURL = privateChatroomModel.info?.item.thumbnailImgUrl else {
-            return
-        }
-        
-        guard let itemTitle = privateChatroomModel.info?.item.title else {
-            return
-        }
-        
-        guard let itemPrice = privateChatroomModel.info?.item.price else {
-            return
-        }
-        
-        self.itemSummary = ItemSummaryInChatroom(url: itemImageURL, title: itemTitle, price: itemPrice)
-        
-        setConstraintsItemSummary()
-    }
-    
-    private func setConstraintsItemSummary() {
-        
-        guard let itemSummary = itemSummary else {
-            return
-        }
-        
-        self.view.addSubview(itemSummary)
-        
-        self.itemSummary?.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate(
-            [
-                itemSummary.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,constant: 40.0),
-                itemSummary.widthAnchor.constraint(equalTo: self.view.widthAnchor),
-                itemSummary.heightAnchor.constraint(equalToConstant: 100.0),
-                itemSummary.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-            ]
-        )
-    }
-    
-    // MARK: NavigationBar (Buttons , title)
-    private func setChatroomTitle() {
-        self.chatroomTitle = UILabel(frame: .zero)
-        self.chatroomTitle?.text = privateChatroomModel.info?.opponentId
-
-        self.chatroomTitle?.font = .systemFont(ofSize: 17.0, weight: .bold)
-        self.chatroomTitle?.textAlignment = .center
-        setConstraintsChatroomTitle()
-    }
-    
-    private func bringButtonsToFront() {
-        guard let backButton = self.backButton else {
-            return
-        }
-        
-        guard let menuButton = self.menuButton else {
-            return
-        }
-        
-        self.view.bringSubviewToFront(backButton)
-        self.view.bringSubviewToFront(menuButton)
-    }
-    
-    private func generateBackButton() {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
-        button.setTitle("뒤로", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 17.0)
-        button.tintColor = .black
-        button.addTarget(self, action: #selector(backButtonTouched), for: .touchUpInside)
-        self.backButton = button
-    }
-    
-    private func generateMenuButton() {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(systemName: "ellipsis"), for: .normal)
-        button.tintColor = .black
-        button.addTarget(self, action:#selector(menuButtonTouched), for: .touchUpInside)
-        self.menuButton = button
-        
-    }
-    
-    private func setConstraintsBackButton() {
-        guard let backButton = self.backButton else {
-            return
-        }
-        
-        self.view.addSubview(backButton)
-        
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
-        backButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
-    }
-    
-    private func setConstraintsMenuButton() {
-        guard let menubutton = self.menuButton else {
-            return
-        }
-        
-        self.view.addSubview(menubutton)
-        
-        menubutton.translatesAutoresizingMaskIntoConstraints = false
-        
-        menubutton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
-        menubutton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
-    }
-    
-    private func setConstraintsChatroomTitle() {
-        guard let chatroomTitle = self.chatroomTitle else {
-            return
-        }
-        
-        guard let backButton = self.backButton else {
-            return
-        }
-        
-        guard let text = chatroomTitle.text else {
-            return
-        }
-        
-        let width: CGFloat = round(CGFloat(text.count) * 15.0)
-        
-        self.view.addSubview(chatroomTitle)
-        
-        chatroomTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate(
-            [
-                chatroomTitle.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-                chatroomTitle.centerYAnchor.constraint(equalTo: backButton.centerYAnchor),
-                chatroomTitle.widthAnchor.constraint(equalToConstant: width),
-                chatroomTitle.heightAnchor.constraint(equalTo: backButton.heightAnchor)
-            ]
-        )
-    }
-    
-    private func hideNavigationBar() {
-        navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-    
-    @objc private func backButtonTouched() {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    @objc private func menuButtonTouched() {
-        
-    }
-}
+//    private func commonInit() {
+//        self.view.backgroundColor = .systemBackground
+//        generateBackButton()
+//        generateMenuButton()
+//        setConstraintsBackButton()
+//        setConstraintsMenuButton()
+//        didUpdateModel()
+//        
+//    }
+//    
+//    private func readyToChat() {
+//        
+//        guard let roomId = privateChatroomModel.info?.chatroomId else {
+//            return
+//        }
+//        
+//        guard let sender = UserInfoManager.shared.userInfo?.memberId else {
+//            return
+//        }
+// 
+//        self.socketManager = SocketManager(roomId: roomId, sender: sender)
+//        //socketManager.sendTest(message: "안녕")
+//    }
+//    
+//    
+//    
+//    private func didUpdateModel() {
+//        
+//        setChatroomTitle()
+//        setItemSummary()
+//    }
+//    
+//    private func setItemSummary() {
+//        guard let itemImageURL = privateChatroomModel.info?.item.thumbnailImgUrl else {
+//            return
+//        }
+//        
+//        guard let itemTitle = privateChatroomModel.info?.item.title else {
+//            return
+//        }
+//        
+//        guard let itemPrice = privateChatroomModel.info?.item.price else {
+//            return
+//        }
+//        
+//        self.itemSummary = ItemSummaryInChatroom(url: itemImageURL, title: itemTitle, price: itemPrice)
+//        
+//        setConstraintsItemSummary()
+//    }
+//    
+//    private func setConstraintsItemSummary() {
+//        
+//        guard let itemSummary = itemSummary else {
+//            return
+//        }
+//        
+//        self.view.addSubview(itemSummary)
+//        
+//        self.itemSummary?.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        NSLayoutConstraint.activate(
+//            [
+//                itemSummary.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,constant: 40.0),
+//                itemSummary.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+//                itemSummary.heightAnchor.constraint(equalToConstant: 100.0),
+//                itemSummary.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+//            ]
+//        )
+//    }
+//    
+//    // MARK: NavigationBar (Buttons , title)
+//    private func setChatroomTitle() {
+//        self.chatroomTitle = UILabel(frame: .zero)
+//        self.chatroomTitle?.text = privateChatroomModel.info?.opponentId
+//
+//        self.chatroomTitle?.font = .systemFont(ofSize: 17.0, weight: .bold)
+//        self.chatroomTitle?.textAlignment = .center
+//        setConstraintsChatroomTitle()
+//    }
+//    
+//    private func bringButtonsToFront() {
+//        guard let backButton = self.backButton else {
+//            return
+//        }
+//        
+//        guard let menuButton = self.menuButton else {
+//            return
+//        }
+//        
+//        self.view.bringSubviewToFront(backButton)
+//        self.view.bringSubviewToFront(menuButton)
+//    }
+//    
+//    private func generateBackButton() {
+//        let button = UIButton(type: .system)
+//        button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+//        button.setTitle("뒤로", for: .normal)
+//        button.titleLabel?.font = .systemFont(ofSize: 17.0)
+//        button.tintColor = .black
+//        button.addTarget(self, action: #selector(backButtonTouched), for: .touchUpInside)
+//        self.backButton = button
+//    }
+//    
+//    private func generateMenuButton() {
+//        let button = UIButton(type: .system)
+//        button.setImage(UIImage(systemName: "ellipsis"), for: .normal)
+//        button.tintColor = .black
+//        button.addTarget(self, action:#selector(menuButtonTouched), for: .touchUpInside)
+//        self.menuButton = button
+//        
+//    }
+//    
+//    private func setConstraintsBackButton() {
+//        guard let backButton = self.backButton else {
+//            return
+//        }
+//        
+//        self.view.addSubview(backButton)
+//        
+//        backButton.translatesAutoresizingMaskIntoConstraints = false
+//        backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+//        backButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+//    }
+//    
+//    private func setConstraintsMenuButton() {
+//        guard let menubutton = self.menuButton else {
+//            return
+//        }
+//        
+//        self.view.addSubview(menubutton)
+//        
+//        menubutton.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        menubutton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+//        menubutton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+//    }
+//    
+//    private func setConstraintsChatroomTitle() {
+//        guard let chatroomTitle = self.chatroomTitle else {
+//            return
+//        }
+//        
+//        guard let backButton = self.backButton else {
+//            return
+//        }
+//        
+//        guard let text = chatroomTitle.text else {
+//            return
+//        }
+//        
+//        let width: CGFloat = round(CGFloat(text.count) * 15.0)
+//        
+//        self.view.addSubview(chatroomTitle)
+//        
+//        chatroomTitle.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        NSLayoutConstraint.activate(
+//            [
+//                chatroomTitle.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+//                chatroomTitle.centerYAnchor.constraint(equalTo: backButton.centerYAnchor),
+//                chatroomTitle.widthAnchor.constraint(equalToConstant: width),
+//                chatroomTitle.heightAnchor.constraint(equalTo: backButton.heightAnchor)
+//            ]
+//        )
+//    }
+//    
+//    private func hideNavigationBar() {
+//        navigationController?.setNavigationBarHidden(true, animated: true)
+//    }
+//    
+//    @objc private func backButtonTouched() {
+//        navigationController?.popViewController(animated: true)
+//    }
+//    
+//    @objc private func menuButtonTouched() {
+//        
+//    }
+//}
--- iOS/second-hand/second-hand/Network/SocketManager.swift
+++ iOS/second-hand/second-hand/Network/SocketManager.swift
@@ -1,89 +1,89 @@
+////
+////  StompClient.swift
+////  second-hand
+////
+////  Created by SONG on 2023/07/18.
+////
 //
-//  StompClient.swift
-//  second-hand
+//import Foundation
+//import StompClientLib
+//import SwiftStomp
 //
-//  Created by SONG on 2023/07/18.
+//class SocketManager {
+//    var socketClient : SwiftStomp?
+//    private var roomId: String
+//    private var sender: String
 //
-
-import Foundation
-import StompClientLib
-import SwiftStomp
-
-class SocketManager {
-    var socketClient : SwiftStomp?
-    private var roomId: String
-    private var sender: String
-    
-    init(roomId: String, sender: String) {
-        self.roomId = roomId
-        self.sender = sender
-        
-        guard let socketURL = URL(string: "ws://3.37.51.148:81/chat") else {
-            return
-        }
-        
-        self.socketClient = SwiftStomp(host: socketURL)
-        self.socketClient?.delegate = self
-        self.socketClient?.autoReconnect = true
-        self.socketClient?.enableLogging = true
-        self.socketClient?.connect()
-    }
-    
-    func send(message: String) {
-        guard let body = JSONCreater().createWSMessageRequestBody(roomId: self.roomId, sender: self.sender, message: message) else {
-            return
-        }
-        
-        let header = ["content-type":"application/json;charset=UTF-8"]
-
-        let bodyToSend = removeBackslashes(from: String(data: body, encoding: .utf8)!)
-        socketClient?.send(body:bodyToSend, to: "/pub/message",headers: header)
-        
-    }
-    
-    private func makeRequestForm(roomId: String, memberId: String, message: String) {
-        
-    }
-    
-    func disconnect() {
-        socketClient?.disconnect()
-        //unsubscribe는 disconnect 이전에 한번 해주자.
-    }
-}
-
-extension SocketManager : SwiftStompDelegate {
-    func onConnect(swiftStomp: SwiftStomp, connectType: StompConnectType) {
-        print("소켓열림")
-        socketClient?.subscribe(to: "/sub/\(self.roomId)")
-    }
-    
-    func onDisconnect(swiftStomp: SwiftStomp, disconnectType: StompDisconnectType) {
-        print("소켓닫힘")
-    }
-    
-    func onMessageReceived(swiftStomp: SwiftStomp, message: Any?, messageId: String, destination: String, headers: [String : String]) {
-        print("뭐 왔다")
-    }
-    
-    func onReceipt(swiftStomp: SwiftStomp, receiptId: String) {
-        print("영수증")
-    }
-    
-    func onError(swiftStomp: SwiftStomp, briefDescription: String, fullDescription: String?, receiptId: String?, type: StompErrorType) {
-        print("에러")
-    }
-    
-    func onSocketEvent(eventName: String, description: String) {
-        print(eventName)
-        print(description)
-    }
-    
-    func removeBackslashes(from jsonString: String) -> String {
-        let pattern = #"\\(.{1})"#
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        let range = NSRange(location: 0, length: jsonString.utf16.count)
-        
-        return regex.stringByReplacingMatches(in: jsonString, options: [], range: range, withTemplate: "$1")
-    }
-    
-}
+//    init(roomId: String, sender: String) {
+//        self.roomId = roomId
+//        self.sender = sender
+//
+//        guard let socketURL = URL(string: "ws://3.37.51.148:81/chat") else {
+//            return
+//        }
+//
+//        self.socketClient = SwiftStomp(host: socketURL)
+//        self.socketClient?.delegate = self
+//        self.socketClient?.autoReconnect = true
+//        self.socketClient?.enableLogging = true
+//        self.socketClient?.connect()
+//    }
+//
+//    func send(message: String) {
+//        guard let body = JSONCreater().createWSMessageRequestBody(roomId: self.roomId, sender: self.sender, message: message) else {
+//            return
+//        }
+//
+//        let header = ["content-type":"application/json;charset=UTF-8"]
+//
+//        let bodyToSend = removeBackslashes(from: String(data: body, encoding: .utf8)!)
+//        socketClient?.send(body:bodyToSend, to: "/pub/message",headers: header)
+//
+//    }
+//
+//    private func makeRequestForm(roomId: String, memberId: String, message: String) {
+//
+//    }
+//
+//    func disconnect() {
+//        socketClient?.disconnect()
+//        //unsubscribe는 disconnect 이전에 한번 해주자.
+//    }
+//}
+//
+//extension SocketManager : SwiftStompDelegate {
+//    func onConnect(swiftStomp: SwiftStomp, connectType: StompConnectType) {
+//        print("소켓열림")
+//        socketClient?.subscribe(to: "/sub/\(self.roomId)")
+//    }
+//
+//    func onDisconnect(swiftStomp: SwiftStomp, disconnectType: StompDisconnectType) {
+//        print("소켓닫힘")
+//    }
+//
+//    func onMessageReceived(swiftStomp: SwiftStomp, message: Any?, messageId: String, destination: String, headers: [String : String]) {
+//        print("뭐 왔다")
+//    }
+//
+//    func onReceipt(swiftStomp: SwiftStomp, receiptId: String) {
+//        print("영수증")
+//    }
+//
+//    func onError(swiftStomp: SwiftStomp, briefDescription: String, fullDescription: String?, receiptId: String?, type: StompErrorType) {
+//        print("에러")
+//    }
+//
+//    func onSocketEvent(eventName: String, description: String) {
+//        print(eventName)
+//        print(description)
+//    }
+//
+//    func removeBackslashes(from jsonString: String) -> String {
+//        let pattern = #"\\(.{1})"#
+//        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+//        let range = NSRange(location: 0, length: jsonString.utf16.count)
+//
+//        return regex.stringByReplacingMatches(in: jsonString, options: [], range: range, withTemplate: "$1")
+//    }
+//
+//}
