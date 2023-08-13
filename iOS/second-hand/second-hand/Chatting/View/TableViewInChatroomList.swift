3f4be45928bfbdf58bf0884f45bc6dc5ff8fecce
SONG <geun2121@gmail.com>
2023-08-13 17:11:29 +0900

    [#374] FEAT: tableView 기본 속성/ 메소드 정의
    - delegate , dataSource
    - init
    - havingCell

--- iOS/second-hand/second-hand/Chatting/View/TableViewInChatroomList.swift
+++ iOS/second-hand/second-hand/Chatting/View/TableViewInChatroomList.swift
@@ -8,7 +8,29 @@
 import UIKit
 
 class TableViewInChatroomList: UITableView {
+    private var havingCell : [CellOfChatroomList] = []
+    
+    init(chatroomList:ChatroomListModel) {
+        super.init(frame: .zero, style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
 
+extension TableViewInChatroomList : UITableViewDelegate {
+    
+}
 
-
+extension TableViewInChatroomList : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return CellOfChatroomList.init(style: .default, reuseIdentifier: CellOfChatroomList.identifier)
+    }
+    
+    
 }
