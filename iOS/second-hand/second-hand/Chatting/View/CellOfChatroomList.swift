e38248f7e1224f478854f1d8732dd1070bade6b5
SONG <geun2121@gmail.com>
2023-08-13 17:05:21 +0900

    [#374] ADD: 채팅리스트 파일 추가
    - model , Cell, TableView 파일 추가

---
+++ iOS/second-hand/second-hand/Chatting/View/CellOfChatroomList.swift
@@ -0,0 +1,23 @@
+//
+//  CellOfChatroomList.swift
+//  second-hand
+//
+//  Created by SONG on 2023/08/13.
+//
+
+import UIKit
+
+class CellOfChatroomList: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS/second-hand/second-hand/Chatting/View/TableViewInChatroomList.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewInChatrromList.swift
+//  second-hand
+//
+//  Created by SONG on 2023/08/13.
+//
+
+import UIKit
+
+class TableViewInChatroomList: UITableView {
+
+
+
+}
