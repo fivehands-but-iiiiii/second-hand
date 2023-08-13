4b6e3dcba8bb017cabe81d0a7fd6ef9823f9cda5
SONG <geun2121@gmail.com>
2023-08-13 17:08:23 +0900

    [#374] FEAT: updatable 프로토콜 채택
    - ChatroomListModel 에 updatable 프로토콜 채택

--- iOS/second-hand/second-hand/Chatting/Model/ChatroomListModel.swift
+++ iOS/second-hand/second-hand/Chatting/Model/ChatroomListModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class ChatroomListModel: Updatable {
+    var info : [ChatroomList] = []
+    
+    func updateData<T>(from fetchedData: T) where T : Decodable, T : Encodable {
+        guard let response = fetchedData as? ChatroomList else{
+            print("캐스팅실패")
+            return
+        }
+        self.info.append(response)
+    }
+}
