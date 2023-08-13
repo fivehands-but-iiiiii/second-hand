b9fad954ab7fa8d3480f43882f9a13404dcf9db0
SONG <geun2121@gmail.com>
2023-08-13 17:14:45 +0900

    [#374] FEAT: 채팅목록 뷰컨트롤러 흐름 구현
    - 모델 데이터 GET
    - 모델 업데이트 이후 모델 기반으로 테이블뷰 생성 예정

--- iOS/second-hand/second-hand/Chatting/Controller/ChattingViewController.swift
+++ iOS/second-hand/second-hand/Chatting/Controller/ChattingViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 final class ChattingViewController: NavigationUnderLineViewController {
-
+    private var chatroomListTableView : TableViewInChatroomList? = nil
+    private var chatroomListModel = ChatroomListModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "채팅"
     }
 
+    private func setupList() {
+        if !UserInfoManager.shared.isLogOn {
+            
+        } else {
+            fetchChatroomListData()
+        }
+    }
+    
+    private func fetchChatroomListData() {
+        guard let url = URL(string: Server.shared.createRequestURLToChatroomList(page: 0, itemId: 199)) else {
+            return
+        }
+        
+        NetworkManager.sendGET(decodeType: ChatroomListSuccess.self, what: nil, fromURL: url) { (result: Result<[ChatroomListSuccess], Error>) in
+            switch result {
+            case .success(let response) :
+                guard let chatroomListPage = response.last?.chatRooms else {
+                    return
+                }
+                self.chatroomListModel.updateData(from: chatroomListPage)
+                //chatroomListModel.info[index] index가 page 넘버
+                
+            case .failure(let error) :
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func makeTableView() {
+        self.chatroomListTableView = TableViewInChatroomList(frame: .zero)
+        print
+    }
 }
