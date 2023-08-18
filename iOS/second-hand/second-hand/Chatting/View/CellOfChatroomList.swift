4c37e4fa8a43de813b1370392bf43fbc382f9320
SONG <geun2121@gmail.com>
2023-08-18 16:41:19 +0900

    [#374] FEAT: cell 초기 세팅 정의
    - 내부 객체들을 초기화시켜주는 메소드 호출

--- iOS/second-hand/second-hand/Chatting/View/CellOfChatroomList.swift
+++ iOS/second-hand/second-hand/Chatting/View/CellOfChatroomList.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class CellOfChatroomList: UITableViewCell {
     static let identifier = "CELLOFLIST"
+    private var opponentImage : UIImageView? = nil
+    private var textSection : TextSectionViewInChatroomListCell? = nil
+    private var numberBadge : UILabel? = nil
+    private var itemImage : UIImageView? = nil
+    
+    init(chatroomList: ChatroomList) {
+        super.init(style: .default, reuseIdentifier: CellOfChatroomList.identifier)
+        self.layer.borderColor = UIColor.neutralOveray.cgColor
+        self.layer.borderWidth = 0.8
+        
+        setOpponentPhoto(img:chatroomList.opponent.profileImgUrl)
+        
+        setTextSection(opponeneId: chatroomList.opponent.memberId, time: chatroomList.chatLogs?.updatedAt ?? "", lastMessage: chatroomList.chatLogs?.lastMessage ?? "last Message")
+        
+        setNumberBadge(number: chatroomList.chatLogs?.unReadCount ?? 7)
+        
+        setItemPhoto(img: chatroomList.item.thumbnailImgUrl)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        setConstraintsOpponentImage()
+        setConstraintTextSection()
+        setConstraintNumberBadge()
+        setConstraintsItemImage()
+        setCornerRadius()
+    }
 
 
     override func setSelected(_ selected: Bool, animated: Bool) {
