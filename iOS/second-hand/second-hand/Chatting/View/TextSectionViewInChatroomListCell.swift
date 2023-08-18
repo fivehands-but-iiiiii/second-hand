8893cfd8263452cc9a1c83194ce37d94d0f918d1
SONG <geun2121@gmail.com>
2023-08-18 16:36:10 +0900

    [#374] FEAT: TextSection 초기세팅
    - 프로퍼티 정의
    - 초기화 메소드 정의
    - layoutSubview 재정의

--- iOS/second-hand/second-hand/Chatting/View/TextSectionViewInChatroomListCell.swift
+++ iOS/second-hand/second-hand/Chatting/View/TextSectionViewInChatroomListCell.swift
@@ -7,8 +7,27 @@
 
 import UIKit
 
-class TextSectionVIewInChatroomListCell: UIView {
-
+class TextSectionViewInChatroomListCell: UIView {
+    private var opponentIdLabel : UILabel? = nil
+    private var timeLabel : UILabel? = nil
+    private var lastMessage : UILabel? = nil
+    
+    init(opponeneId: String, time: String, lastMessage: String) {
+        super.init(frame: .zero)
+        setOpponentIdLabel(opponeneId)
+        setTimeLabel(time)
+        setLastMessage(lastMessage)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        setConstraintsOpponentIdLabel()
+        setConstraintsTimeLabel()
+        setConstraintsLastMessage()
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
