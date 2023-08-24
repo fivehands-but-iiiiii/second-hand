8225383af76f90bdd785fd3464c3d73eedd9f699
Haena <ghkwlsspdl@naver.com>
2023-08-24 16:25:58 +0900

    [#387] ADD: Tag스트럭트 추가 - 아이템 등록시 사진 삭제버튼을 클릭하면 태그와 데이터를 매핑 시켜서 삭제시키기 위해 모델형식의 데이터 추가

---
+++ iOS/second-hand/second-hand/Home/subScene/RegisterNewProduct/Model/Tag.swift
@@ -0,0 +1,15 @@
+//
+//  Tag.swift
+//  second-hand
+//
+//  Created by leehwajin on 2023/08/24.
+//
+
+import Foundation
+
+struct Tag {
+    var number = 0
+    mutating func nextNumberSetting() {
+        number += 1
+    }
+}
