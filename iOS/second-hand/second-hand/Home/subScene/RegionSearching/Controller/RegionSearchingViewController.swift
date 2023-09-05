86f05546dd23062a5322c1b192a6871f8874dd30
SONG <geun2121@gmail.com>
2023-09-05 17:12:33 +0900

    [#401] FEAT: 지역검색 뷰컨트롤러 속성 구현
    - 내비게이션바 , 컬렉션뷰 선언

--- iOS/second-hand/second-hand/Home/subScene/RegionSearching/Controller/RegionSearchingViewController.swift
+++ iOS/second-hand/second-hand/Home/subScene/RegionSearching/Controller/RegionSearchingViewController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class RegionSearchingViewController: UIViewController {
     
+    let regionsController = RegionController()
+    let searchBar = UISearchBar(frame: .zero)
+    var regionsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         commonInit()
+        navigationItem.title = "동네 찾기"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
     }
     
     private func commonInit() {
         self.view.backgroundColor = .white
+        let backButton = UIBarButtonItem(title: "뒤로", style: .plain, target: self, action: #selector(backButtonTapped))
+        backButton.tintColor = .black
+        navigationItem.leftBarButtonItem = backButton
+    }
+
+}
     }
     
     @objc func backButtonTapped() {
