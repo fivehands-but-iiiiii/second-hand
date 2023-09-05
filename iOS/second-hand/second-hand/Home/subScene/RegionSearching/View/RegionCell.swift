4096dbe938a02efb001ccf7e87a823d14c459f0a
SONG <geun2121@gmail.com>
2023-09-05 17:30:47 +0900

    [#401] FEAT: RegionCell 구현
    - 화면에 표시될 cell View 구현

--- iOS/second-hand/second-hand/Home/subScene/RegionSearching/View/RegionCell.swift
+++ iOS/second-hand/second-hand/Home/subScene/RegionSearching/View/RegionCell.swift
@@ -8,5 +8,34 @@
 import UIKit
 
 class RegionCell: UICollectionViewCell {
-    
+    static let reuseIdentifier = "REGIONCELL"
+    let label = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
 }
+
+extension RegionCell {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(label)
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.adjustsFontForContentSizeCategory = true
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemGray2.cgColor
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+            ])
+    }
+}
+
+
