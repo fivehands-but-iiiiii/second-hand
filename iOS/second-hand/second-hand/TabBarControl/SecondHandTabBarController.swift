9a82ae977bdb64c69a8eca23d4c876f273eb651c
SONG <geun2121@gmail.com>
2023-09-09 16:43:35 +0900

    [#401] FEAT: 로그오프 유저가 접근할 수 있는 화면 제한
    - 로그오프 상태인 유저가 특정 탭에 접근 할 수 없도록 alert을 띄운다

--- iOS/second-hand/second-hand/TabBarControl/SecondHandTabBarController.swift
+++ iOS/second-hand/second-hand/TabBarControl/SecondHandTabBarController.swift
@@ -18,6 +18,7 @@ class SecondHandTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
         addViewController()
         tabBar.tintColor = .black
     }
@@ -37,9 +38,21 @@ class SecondHandTabBarController: UITabBarController {
 }
 
 extension SecondHandTabBarController: UITabBarControllerDelegate {
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        if item.title == TabBarItemType.myAccount.rawValue {
-            
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let title = viewController.tabBarItem.title
+        
+        if !UserInfoManager.shared.isLogOn {
+            switch title {
+            case "판매내역", "관심목록", "채팅":
+                let alert = UIAlertController(title: "로그인 필요", message: "해당 기능을 사용하려면 로그인이 필요합니다.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return false
+            default :
+                return true
+            }
+        } else {
+            return true
         }
     }
 }
