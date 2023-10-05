39a7f3cf3aac8c4ca6cae0554156464ba68671d4
SONG <geun2121@gmail.com>
2023-10-05 18:39:28 +0900

    [#414] FEAT: region변경 로직 구현
    - BehaviorSubject 로 바뀐 상태를 구독으로 받아올 수 있게 세팅
    - updateData() 에서 변경된 상태를 subject에 전달할 수 있음

--- iOS/second-hand/second-hand/Network/UserInfoManager.swift
+++ iOS/second-hand/second-hand/Network/UserInfoManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RxSwift
 
 class UserInfoManager: Updatable{
     static let shared = UserInfoManager()
@@ -13,17 +14,28 @@ class UserInfoManager: Updatable{
     var isLogOn: Bool = false
     var loginToken: String? = nil
     
+    let regionSubject = BehaviorSubject<Region>(value: Region(id: 2729060200, onFocus: true, district: "월성2동"))
+    
     private let loginNotification = Notification(name: NSNotification.Name("LOGIN"))
 
-    
     func updateData<T:Codable>(from fetchedData: T) {
         
         guard let data = fetchedData as? LoginFetchedUserData else {
             return
         }
+        
+        guard let region = data.regions.first else {
+            return
+        }
         self.userInfo = data
         isLogOn = true
+        
+        regionSubject.onNext(region)
         NotificationCenter.default.post(name: loginNotification.name, object: nil)
     }
+    
+    func loadRegion() -> Observable<Region> {
+            return regionSubject.asObservable()
+        }
 }
 
